f89a82aa70c68bc982024ca43f8f2b725b78a0a3
Adrien <adrienquijo@gmail.com>
2025-10-28 18:49:32 +0100

    Le boutton intègre maintenant un href pour permettre la navigation si l'élément n'est pas un submenu

--- src/components/Sidebar/nav/elements/menu_button/PrimaryMenuButton.tsx
+++ src/components/Sidebar/nav/elements/menu_button/PrimaryMenuButton.tsx
@@ -1,6 +1,8 @@
 import type { MenuContentProps } from "@/components/Sidebar/nav/types/NavTypes.ts";
 import { SidebarMenuButton } from "@/components/ui/sidebar.tsx";
+import "@css/SidebarButtons.scss";
 import { ChevronRight } from "lucide-react";
+import { Link } from "react-router-dom";
 
 /**
  * Menu button component for sidebar menu items.
@@ -9,7 +11,7 @@ import { ChevronRight } from "lucide-react";
  * @description This can be used as the trigger for collapsible menu items in the sidebar.
  *
  * @param item Menu item to display
- * @param setStyle Function to set style
+ * @param setStyle Function to set style&
  */
 export default function PrimaryMenuButton({
   item,
@@ -20,19 +22,20 @@ export default function PrimaryMenuButton({
   const isQuickButtonEnabled = item.quickButton?.enabled;
 
   return (
-    <SidebarMenuButton
-      {...props}
-      className={`${setStyle({ isQuickButtonEnabled, isMenu: false })} ${
-        props.className ?? ""
-      }`}
-      title={item.tooltip}
-    >
-      {item.icon && <item.icon />}
-      <p>{item.title}</p>
-
-      {item.subMenus && (
-        <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
-      )}
-    </SidebarMenuButton>
+    <>
+      <Link to={item.url ?? "#"} className="w-full">
+        <SidebarMenuButton
+          {...props}
+          className={`${setStyle({ isQuickButtonEnabled, isMenu: false })} ${
+            props.className ?? ""
+          }`}
+          title={item.tooltip}
+        >
+          {item.icon && <item.icon style={{ stroke: "#404040" }} />}
+          <p>{item.title}</p>
+          {item.subMenus && <ChevronRight className="sidebarButton--submenu" />}
+        </SidebarMenuButton>
+      </Link>
+    </>
   );
 }
