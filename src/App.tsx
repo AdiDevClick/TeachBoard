9c0bec203b0f7f85256455b1133ca001b38214be
Adrien <adrienquijo@gmail.com>
2025-10-28 23:34:43 +0100

    En fonction de la route en cours, le titre à passer au H1 sera mis à jour

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,27 @@
 import { PageTitle } from "@/components/Header/PageTitle.tsx";
 import "@css/index-tailwind.css";
 import "@css/PageTitle.scss";
-import type { ReactNode } from "react";
+import { type ReactNode } from "react";
+import { useLocation, useMatches } from "react-router-dom";
 
 /**
  * App component to wrap all pages
+ *
+ * @description This component serves as the main wrapper for all pages, providing a consistent layout with a page title.
+ *
+ * @param pageTitle Title of the page
+ * @param children Page content
  */
 export default function App({ children }: { children: ReactNode }) {
+  const location = useLocation().pathname;
+  const matches = useMatches().find(
+    (m) => m.loaderData && m.pathname === location
+  );
+  const title = matches ? matches.loaderData?.pageTitle : "TeachBoard";
+
   return (
     <>
-      <PageTitle>My App</PageTitle>
+      <PageTitle>{title}</PageTitle>
       {children}
     </>
   );
