b7365cd5bd2a77b62610117aee7b9b3d62ec003a
Adrien <adrienquijo@gmail.com>
2025-10-28 10:27:18 +0100

    Création d'un hook qui permet de wrap le useContext de SidebarData et éviter une boilerplate

---
+++ src/hooks/contexts/useSidebarDataContext.ts
@@ -0,0 +1,19 @@
+import { SidebarDataContext } from "@/api/contexts/SidebarDataContext.ts";
+import { use } from "react";
+
+/**
+ * Custom hook to use SidebarDataContext
+ *
+ * @throws Error if used outside of SidebarDataProvider
+ */
+export const useSidebarDataContext = () => {
+  const context = use(SidebarDataContext);
+
+  if (!context) {
+    throw new Error(
+      "useSidebarDataContext must be used within a SidebarDataProvider"
+    );
+  }
+
+  return context;
+};
