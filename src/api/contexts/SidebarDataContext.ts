220bd8eaab462ccb49f71d3fa994d4c5a1816dc7
Adrien <adrienquijo@gmail.com>
2025-10-28 10:25:13 +0100

    Modification JSDoc

--- src/api/contexts/SidebarDataContext.ts
+++ src/api/contexts/SidebarDataContext.ts
@@ -2,7 +2,7 @@ import type { CompleteDatas } from "@/main.tsx";
 import { createContext } from "react";
 
 /**
- * Context for Sidebar Data
+ * Context for Sidebar Data Provider
  */
 export const SidebarDataContext = createContext<typeof CompleteDatas | null>(
   null
