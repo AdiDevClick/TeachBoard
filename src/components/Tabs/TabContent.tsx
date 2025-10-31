055a9a3bc69071c58b3c86b0938c24039a67180d
AdiDevClick <adrienquijo@gmail.com>
2025-10-31 11:14:57 +0100

    Utilisation d'une feuille de style - Code cleanup -

--- src/components/Tabs/TabContent.tsx
+++ src/components/Tabs/TabContent.tsx
@@ -1,3 +1,4 @@
+import "@css/EvaluationPage.scss";
 import { TabsContent } from "@radix-ui/react-tabs";
 import { IconArrowLeft, IconArrowRightDashed } from "@tabler/icons-react";
 import {
@@ -11,42 +12,48 @@ import {
 import VerticalFieldSelect from "../ui/select-vertical.tsx";
 import { SelectGroup, SelectItem, SelectLabel } from "../ui/select.tsx";
 import { Separator } from "../ui/separator.tsx";
+import { useSidebar } from "../ui/sidebar.tsx";
 
 export function TabContent({ value, children, handleOnClick }) {
+  const { isMobile } = useSidebar();
+
   return (
     <TabsContent
       value={value}
-      className="page-content"
-      style={{ height: "100%" }}
+      className="evaluation-page-container"
+      // style={{ height: "100%" }}
     >
       <Card
-        className="test"
-        style={{
-          width: "97%",
-          height: "100%",
-          justifySelf: "center",
-          boxShadow: "none",
-          border: "none",
-          borderLeft: "7px solid",
-          display: "flex",
-          flexDirection: "row",
-          // justifyContent: "space-between",
-        }}
+        className="evaluation-page__cards-container"
+        // style={{
+        //   width: "97%",
+        //   height: "100%",
+        //   justifySelf: "center",
+        //   boxShadow: "none",
+        //   border: "none",
+        //   borderLeft: "7px solid",
+        //   display: "flex",
+        //   flexDirection: "row",
+        //   // justifyContent: "space-between",
+        // }}
       >
-        <CardHeader>
+        <CardHeader className="cards-container__header">
           <IconArrowLeft onClick={handleOnClick} data-name="step-previous" />
         </CardHeader>
         {children}
         <CardContent
-          className="flex gap-6 tab-card-content"
-          style={{
-            alignItems: "center",
-            placeSelf: "center",
-            width: "100%",
-            height: "100%",
-          }}
+          className="cards-container__content"
+          // className="flex gap-6 tab-card-content"
+          // style={{
+          //   alignItems: "center",
+          //   // placeSelf: "center",
+          //   width: "100%",
+          //   // height: "100%",
+          //   display: "grid",
+          //   gridTemplateColumns: "1fr 0fr 3fr",
+          // }}
         >
-          <div style={{ width: "100%" }}>
+          <div className="content__left">
             <CardTitle>1.</CardTitle>
             <CardTitle>Sélectionner une classe</CardTitle>
             <CardDescription>
@@ -54,33 +61,40 @@ export function TabContent({ value, children, handleOnClick }) {
             </CardDescription>
           </div>
           <Separator
-            orientation="vertical"
-            style={{ height: "50%", width: 1, margin: "auto" }}
+            className="content__separator"
+            orientation={isMobile ? "vertical" : "horizontal"}
+            // style={{ height: "50%", width: 1, margin: "auto" }}
           />
-          <Card style={{ width: "100%" }}>
+          <Card
+            className="content__right"
+            // style={{ placeSelf: "center", minWidth: "40%" }}
+          >
             {/* <CardHeader>
                   <CardTitle>Les classes disponibles</CardTitle>
                   <CardDescription>Les classes disponibles</CardDescription>
                 </CardHeader> */}
-            <CardContent className="grid gap-6">
-              <div className="grid gap-3">
-                <VerticalFieldSelect
-                  placeholder="Sélectionner une classe"
-                  onValueChange={(value) => console.log(value)}
-                  label="Les classes disponibles"
-                >
-                  <SelectGroup>
-                    <SelectLabel>BTS</SelectLabel>
-                    <SelectItem value="class-1ereA">
-                      1ère A - Sciences de l'Ingénieur
-                    </SelectItem>
-                  </SelectGroup>
-                </VerticalFieldSelect>
-              </div>
+            <CardContent className="right__content-container">
+              {/* <CardContent className="grid gap-6"> */}
+              {/* <div className="grid gap-3"> */}
+              <VerticalFieldSelect
+                className="right__content"
+                // className="grid gap-3"
+                placeholder="Sélectionner une classe"
+                onValueChange={(value) => console.log(value)}
+                label="Les classes disponibles"
+              >
+                <SelectGroup>
+                  <SelectLabel>BTS</SelectLabel>
+                  <SelectItem value="class-1ereA">
+                    1ère A - Sciences de l'Ingénieur
+                  </SelectItem>
+                </SelectGroup>
+              </VerticalFieldSelect>
+              {/* </div> */}
             </CardContent>
           </Card>
         </CardContent>
-        <CardFooter>
+        <CardFooter className="cards-container__footer">
           <IconArrowRightDashed onClick={handleOnClick} data-name="next-step" />
         </CardFooter>
       </Card>
