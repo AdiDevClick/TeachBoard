4d42ad3d21ed1832ac37c9d5c546b59751a7b03a
Adrien <adrienquijo@gmail.com>
2025-11-04 22:16:27 +0100

    Ajout d'un hook permettant d'importer dynamiquement des SVG - Il prend en paramètre un objet composé d'un name, path et url

---
+++ src/hooks/useDynamicSVGImport.ts
@@ -0,0 +1,52 @@
+import type { UseDynamicSVGImportTypes } from "@/hooks/types/useDynamicSVGImportTypes.ts";
+import { useEffect, useState, type ComponentType, type SVGProps } from "react";
+
+const icons = import.meta.glob("/src/assets/icons/*.svg", {
+  import: "default",
+});
+
+/**
+ * Custom hook to dynamically import SVG icons.
+ *
+ * @param icon - The icon to import.
+ * @param options - Additional options for the import.
+ */
+export function useDynamicSVGImport({
+  icon,
+  options = {},
+}: UseDynamicSVGImportTypes) {
+  const [SvgIcon, setSvgIcon] = useState<
+    ComponentType<SVGProps<SVGSVGElement>>
+  >(null!);
+  const [error, setError] = useState("");
+
+  /**
+   * Dynamically imports the SVG icon based on the provided path.
+   *
+   * @description This uses the `import.meta.glob` to find the icon file and imports it.
+   */
+  useEffect(() => {
+    const importIcon = async () => {
+      try {
+        if (icon == null) {
+          throw new Error("Icon is undefined");
+        }
+
+        const key = `/src/assets/icons/${icon.path}.svg`;
+
+        if (!icons[key]) {
+          throw new Error(`Icon not found: ${key}`);
+        }
+
+        const path = key + "?react";
+        const module = await import(/* @vite-ignore */ path);
+        setSvgIcon(() => module.default);
+      } catch (error) {
+        setError((error as Error).message);
+      }
+    };
+    importIcon();
+  }, [icon]);
+
+  return { SvgIcon, error };
+}
