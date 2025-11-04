9eb81091d357910b71ba71d25234b0bc1fa1509d
Adrien <adrienquijo@gmail.com>
2025-11-04 22:17:43 +0100

    Ajout d'un composant important dynamiquement un svg via le hook d'importantion -

---
+++ src/components/Icons/Icon.tsx
@@ -0,0 +1,25 @@
+import type { IconPropsTypes } from "@/components/Icons/types/IconTypes.ts";
+import { useDynamicSVGImport } from "@/hooks/useDynamicSVGImport.ts";
+
+/**
+ * Renders an SVG icon.
+ *
+ * @description This component dynamically imports an SVG icon based on the provided path.
+ * It uses the `useDynamicSVGImport` hook to handle the import and loading state.
+ *
+ * @param icon - Object containing the icon's path and other properties.
+ * @param props - Additional properties to pass to the SVG element.
+ */
+export function Icon({ icon, ...props }: IconPropsTypes) {
+  const { SvgIcon, error } = useDynamicSVGImport({ icon });
+
+  if (error) {
+    return <div>Can't load the icon</div>;
+  }
+
+  if (SvgIcon) {
+    return <SvgIcon className="social__icon" {...props} />;
+  }
+
+  return <div>Loading icon...</div>;
+}
