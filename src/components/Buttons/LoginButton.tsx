a9189630b11ef31704ae92907e3a6424f30490e2
Adrien <adrienquijo@gmail.com>
2025-11-04 22:14:38 +0100

    Création d'un composant représentant les boutons pour se connecter via les réseaux sociaux

---
+++ src/components/Buttons/LoginButton.tsx
@@ -0,0 +1,21 @@
+import type { LoginButtonProps } from "@/components/Buttons/types/ButtonTypes.ts";
+import { Icon } from "@/components/Icons/Icon.tsx";
+import { Button } from "@/components/ui/button";
+import type { LoginButtonsSvgsType } from "@/configs/social.config.ts";
+
+/**
+ * Login button component
+ * @description Renders a login button with an icon and name.
+ *
+ * @link configs/social.config.ts for the item structure
+ *
+ * @param item - Icon item data
+ */
+export function LoginButton({ ...item }: LoginButtonProps) {
+  return (
+    <Button variant="outline" type="button">
+      <Icon icon={item as LoginButtonsSvgsType} />
+      {item.name}
+    </Button>
+  );
+}
