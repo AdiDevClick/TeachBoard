86986812fc0f26a501e17065c6703d3fffa234ca
Adrien <adrienquijo@gmail.com>
2025-11-04 22:19:09 +0100

    Ajout d'un fichier de config pour la page de login

---
+++ src/configs/social.config.ts
@@ -0,0 +1,21 @@
+/**
+ * Login page social icons.
+ */
+export const loginButtonsSvgs = [
+  {
+    name: "Se connecter avec Apple",
+    path: "apple",
+    url: "https://github.com/AdiDevClick",
+  },
+  {
+    name: "Se connecter avec Microsoft",
+    path: "microsoft",
+    url: "https://linkedin.com/in/adrienquijo",
+  },
+  {
+    name: "Se connecter avec Google",
+    path: "google",
+    url: "https://twitter.com/AdiDevClick",
+  },
+];
+export type LoginButtonsSvgsType = (typeof loginButtonsSvgs)[number];
