f187fc7b543d5aaa73b4a0632fa028ae4821c0e0
Adrien <adrienquijo@gmail.com>
2025-11-06 19:35:12 +0100

    Création d'un fichier représentant les inputs des controllers de la page login

---
+++ src/data/loginInputControllers.ts
@@ -0,0 +1,14 @@
+export const inputControllers = [
+  {
+    name: "identifier",
+    title: "Identifiant",
+    type: "text",
+    placeholder: "m@example.com",
+  },
+  {
+    name: "password",
+    title: "Mot de passe",
+    type: "password",
+    placeholder: "********",
+  },
+];
