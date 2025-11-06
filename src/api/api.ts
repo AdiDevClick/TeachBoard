378c5ef3b59983d5e42b3f3e25a5f312ee14b83e
Adrien <adrienquijo@gmail.com>
2025-11-06 18:31:04 +0100

    Ajout d'une fonction filterErrorResponse() permettant de rajouter correctement le retour générique de l'API en cas d'erreur ou la promesse de l'API en cas de succès -

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,8 @@
-import type { fetchJSONOptions } from "@/api/types/ApiTypes.ts";
+import type {
+  FetchJSONError,
+  fetchJSONOptions,
+  FetchJSONResult,
+} from "@/api/types/ApiTypes.ts";
 
 /**
  * Fetch API to JSON
@@ -9,7 +13,10 @@ import type { fetchJSONOptions } from "@/api/types/ApiTypes.ts";
  * @param options Fetch options. If options.json is provided, it will be sent as JSON body.
  * @returns Parsed JSON response or error cause.
  */
-export async function fetchJSON(url = "", options: fetchJSONOptions = {}) {
+export async function fetchJSON(
+  url = "",
+  options: fetchJSONOptions = {}
+): Promise<FetchJSONResult> {
   const headers = {
     Accept: "application/json",
     ...options.headers,
@@ -22,7 +29,6 @@ export async function fetchJSON(url = "", options: fetchJSONOptions = {}) {
   if (options.json && !options.img) {
     if (Array.isArray(options.json)) {
       options.body = JSON.stringify(Object.fromEntries(options.json));
-      // options.body = Object.fromEntries(options.json);
     } else {
       options.body = JSON.stringify(options.json);
     }
@@ -30,26 +36,52 @@ export async function fetchJSON(url = "", options: fetchJSONOptions = {}) {
   }
 
   try {
-    const response = await fetch(url, { ...options, headers });
+    const response = await fetch(url, {
+      ...options,
+      headers,
+    });
 
     if (!response.ok) {
+      const filteredObject = await filterErrorResponse(response);
+
       throw new Error(
         `HTTP Error ! Status : ${response.status} - ${response.statusText}`,
         {
-          cause: {
-            status: response.status,
-            // message: await response.json(),
-            statusText: response.statusText ?? null,
-            ok: response.ok,
-            ...(await response.json()),
-            // fullResponse: response,
-          },
+          cause: filteredObject,
         }
       );
     }
 
     return { ...(await response.json()), ok: response.ok };
   } catch (error) {
-    return (error as Error).cause;
+    const err = error as Error;
+    return (err.cause as FetchJSONError) ?? err;
+  }
+}
+
+/**
+ * Filter response to extract relevant information
+ *
+ * @param response HTTP response
+ * @returns Promise containing an error object
+ */
+async function filterErrorResponse(
+  response: Response
+): Promise<FetchJSONError> {
+  const cause = {
+    status: response.status,
+    statusText: response.statusText ?? null,
+    ok: response.ok,
+  };
+
+  try {
+    return {
+      ...cause,
+      ...(await response.clone().json()),
+    };
+  } catch {
+    return {
+      ...cause,
+    };
   }
 }
