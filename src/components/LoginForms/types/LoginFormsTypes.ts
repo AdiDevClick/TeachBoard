8f93dfc51009cfee5e84228c6aac5c3407953975
Adrien <adrienquijo@gmail.com>
2025-11-06 19:35:19 +0100

    Ajout de types pour la page de login

--- src/components/LoginForms/types/LoginFormsTypes.ts
+++ src/components/LoginForms/types/LoginFormsTypes.ts
@@ -1,4 +1,22 @@
+import type { inputControllers } from "@/data/loginInputControllers.ts";
+import type { formSchema } from "@/models/login.models.ts";
+import type { ComponentProps } from "react";
+import type z from "zod";
+
 export interface LoginFormData {
   email: string;
   password: string;
+}
+
+export type LoginFormSchema = z.infer<typeof formSchema>;
+
+export type InputController = Omit<
+  (typeof inputControllers)[number],
+  "name"
+> & {
+  name: keyof LoginFormSchema;
+};
+
+export interface LoginFormProps extends ComponentProps<"div"> {
+  inputControllers: InputController[];
 }
