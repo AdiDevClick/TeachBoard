c7cf036decf616b4ea1a24addb3cbfb1ffe3127c
Adrien <adrienquijo@gmail.com>
2025-11-07 17:27:50 +0100

    Le hook peut maintenant, par défaut, faire des retries avec un timeout - Un retry va trigger le message d'erreur sous forme de toaster - Petit fix de zIndex pour les toaster afin qu'ils s'empilent correctement

--- src/hooks/queries/useQueryOnSubmit.ts
+++ src/hooks/queries/useQueryOnSubmit.ts
@@ -1,11 +1,16 @@
 import { fetchJSON } from "@/api/api.ts";
 import { getErrorMessage } from "@/api/errorFunctions.ts";
+import {
+  type FetchJSONError,
+  type FetchJSONSuccess,
+} from "@/api/types/api.types";
 import type {
-  MutationResponse,
   MutationVariables,
   QueryKeyDescriptor,
 } from "@/hooks/queries/types/QueriesTypes.ts";
-import type { ApiError, CustomError } from "@/types/MainTypes.ts";
+import { wait } from "@/lib/utils.ts";
+import type { ApiError } from "@/types/AppErrorInterface.ts";
+import type { ResponseInterface } from "@/types/AppResponseInterface.ts";
 import type { UseMutationOptions } from "@tanstack/react-query";
 import { useMutation } from "@tanstack/react-query";
 import { useCallback } from "react";
@@ -15,20 +20,20 @@ import { toast } from "sonner";
  * Options for the mutation hook.
  * @description Centralizing mutation functions
  */
-const mutationOptions = (
+const mutationOptions = <S extends ResponseInterface, E extends ApiError>(
   queryKeys: QueryKeyDescriptor
 ): UseMutationOptions<
-  MutationResponse,
-  CustomError<ApiError>,
+  FetchJSONSuccess<S>,
+  FetchJSONError<E>,
   MutationVariables
 > => {
   const { url, method = "GET", successDescription } = queryKeys[1];
 
   return {
     mutationKey: queryKeys,
-    mutationFn: (variables) => onFetch(variables, method, url),
+    mutationFn: (variables) => onFetch<S, E>(variables, method, url),
     onSuccess: (response) => onQuerySuccess(response, successDescription),
-    onError: (error) => onQueryError(error),
+    onError: (error) => onQueryError<E>(error),
   };
 };
 
@@ -36,11 +41,13 @@ const mutationOptions = (
  * Handles form submission by triggering the query function.
  *
  * @param queryKeys The query key descriptor containing task and descriptor.
- * @returns An object containing data, isLoading, error, and queryFn.
  */
-export function useQueryOnSubmit(queryKeys: QueryKeyDescriptor) {
+export function useQueryOnSubmit<
+  S extends ResponseInterface,
+  E extends ApiError
+>(queryKeys: QueryKeyDescriptor) {
   const { mutateAsync, data, isPending, error } = useMutation(
-    mutationOptions(queryKeys)
+    mutationOptions<S, E>(queryKeys)
   );
 
   /**
@@ -79,28 +86,43 @@ export function useQueryOnSubmit(queryKeys: QueryKeyDescriptor) {
  * @param variables The variables to be sent in the request body.
  * @returns The mutation response object.
  */
-async function onFetch(
+async function onFetch<
+  TSuccess extends ResponseInterface,
+  TError extends ApiError
+>(
   variables: MutationVariables,
   queryMethod: string,
-  queryUrl?: string
-) {
-  const response = await fetchJSON(getUrl(queryUrl), {
-    method: queryMethod,
-    json: variables,
-  });
-
-  if (!response.ok || response === undefined) {
-    const status = response.status;
-    const message = getErrorMessage(status, response);
-    const apiError = new Error(message, {
-      cause: { status, response },
+  queryUrl?: string,
+  retry = 3,
+  timeout = 1000
+): Promise<FetchJSONSuccess<TSuccess>> {
+  try {
+    const response = await fetchJSON<TSuccess, TError>(getUrl(queryUrl), {
+      method: queryMethod,
+      json: variables,
     });
-    // }) as CustomError<ApiError>;
 
-    throw apiError;
+    if (!response.ok || response === undefined) {
+      const status = response.status;
+      const message = getErrorMessage(status, response, retry);
+
+      throw new Error(message, {
+        cause: { ...response },
+      });
+    }
+
+    // At this point response.ok is true, so cast to the success variant
+    return response;
+  } catch (error) {
+    const errorCause = (error as Error).cause as FetchJSONError<TError>;
+
+    if (shouldRetry(errorCause.status, retry)) {
+      await wait(timeout);
+      return onFetch(variables, queryMethod, queryUrl, retry - 1);
+    }
+
+    throw errorCause;
   }
-
-  return response;
 }
 
 /**
@@ -110,14 +132,15 @@ async function onFetch(
  *
  * @param response The mutation response object.
  */
-function onQuerySuccess(
-  response: MutationResponse,
+function onQuerySuccess<TSuccess extends ResponseInterface>(
+  response: FetchJSONSuccess<TSuccess>,
   querySuccessDescription?: string
 ) {
   const successMessage = response.success ?? undefined;
   toast(successMessage ?? "Success", {
     description: querySuccessDescription,
     position: "top-right",
+    style: { zIndex: 10000 },
   });
 }
 
@@ -128,8 +151,10 @@ function onQuerySuccess(
  *
  * @param error The mutation error object.
  */
-function onQueryError(error: CustomError<ApiError>) {
-  toast.error(error.message);
+function onQueryError<TError extends ApiError>(error: FetchJSONError<TError>) {
+  toast.error(error.message, {
+    style: { zIndex: 10000 },
+  });
 }
 
 /**
@@ -146,4 +171,19 @@ function getUrl(url?: string): string {
   }
 
   return url;
+}
+
+/**
+ * Determine if the request should be retried based on status and remaining retries.
+ *
+ * @param status HTTP status code
+ * @param retry Remaining retry attempts
+ * @returns True if the request should be retried, false otherwise
+ */
+function shouldRetry(status: number, retry: number): boolean {
+  if (typeof status !== "number") {
+    return false;
+  }
+
+  return retry > 0 && status >= 500;
 }
