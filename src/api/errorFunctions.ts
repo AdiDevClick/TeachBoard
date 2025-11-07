e12b7c49f06a8af764612b4d1ec229ab4cd13e3d
Adrien <adrienquijo@gmail.com>
2025-11-07 17:24:36 +0100

    La fonction prend maintenant en compte le nombre de retry qui lui est passé - Elle peut aussi vérifier si le navigateur en cours est online/offline et renvoyer une erreur plus ciblée -

--- src/api/errorFunctions.ts
+++ src/api/errorFunctions.ts
@@ -1,4 +1,5 @@
-import type { ErrorInterface, ErrorMeta } from "@/types/MainTypes.ts";
+import type { ErrorInterface, ErrorMeta } from "@/types/AppErrorInterface.ts";
+import { toast } from "sonner";
 
 /**
  * Get a user-friendly error message based on the HTTP status code and response.
@@ -9,15 +10,31 @@ import type { ErrorInterface, ErrorMeta } from "@/types/MainTypes.ts";
  */
 export function getErrorMessage(
   status?: number,
-  errorPayload?: Partial<ErrorInterface & ErrorMeta> | undefined
+  errorPayload?: Partial<ErrorInterface & ErrorMeta> | undefined,
+  retry?: number
 ): string {
   const fallback =
     "HTTP Error! Please try again later or verify if you are connected to the internet.";
 
   if (!status) {
-    return (
-      errorPayload?.message ?? errorPayload?.error ?? fallback
+    return errorPayload?.message ?? errorPayload?.error ?? fallback;
+  }
+
+  if (!navigator.onLine) {
+    return "You appear to be offline. Please check your internet connection and try again.";
+  }
+
+  if (retry && retry < 3) {
+    toast.info(
+      `A server error occurred. Retrying... (${retry} attempts left)`,
+      {
+        position: "top-right",
+      }
     );
+  }
+
+  if (retry === 0) {
+    return "After multiple attempts, the server is unreachable. Please try again later or contact support.";
   }
 
   const errorMessage = errorPayload?.message ?? errorPayload?.error;
