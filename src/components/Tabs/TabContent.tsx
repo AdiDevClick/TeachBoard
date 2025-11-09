a1fc636dfa084032e439362c147ec3380826fd9a
Adrien <adrienquijo@gmail.com>
2025-11-09 23:27:44 +0100

    code cleanup

--- src/components/Tabs/TabContent.tsx
+++ src/components/Tabs/TabContent.tsx
@@ -1,14 +1,8 @@
+import { LeftSidePageContent } from "@/pages/Evaluations/create/left-content/LeftSidePageContent.tsx";
 import "@css/EvaluationPage.scss";
 import { TabsContent } from "@radix-ui/react-tabs";
 import { IconArrowLeft, IconArrowRightDashed } from "@tabler/icons-react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "../ui/card.tsx";
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card.tsx";
 import { Separator } from "../ui/separator.tsx";
 import { useSidebar } from "../ui/sidebar.tsx";
 
@@ -20,54 +14,29 @@ export function TabContent({ item, index, children, ...props }) {
     arrayLength: props.arrayLength,
     setTabValue: props.setTabValue,
     tabValues: props.tabValues,
-  } as const;
+  };
 
   return (
-    <TabsContent value={item.tabTitle} className="evaluation-page-container">
+    <TabsContent value={item.name} className="evaluation-page-container">
       <Card className="evaluation-page__cards-container">
         <CardHeader className="cards-container__header">
           <IconArrowLeft
-            onClick={(e) => props.handleOnClick({ e, clickProps })}
+            onClick={(e) => props.onClick({ e, ...clickProps })}
             data-name="step-previous"
           />
         </CardHeader>
         <CardContent className="cards-container__content">
-          <div className="content__left">
-            <CardTitle>{leftSide.number}.</CardTitle>
-            <CardTitle>{leftSide.title}</CardTitle>
-            <CardDescription>{leftSide.description}</CardDescription>
-          </div>
+          <LeftSidePageContent item={leftSide} />
           <Separator
             className="content__separator"
             orientation={isMobile ? "vertical" : "horizontal"}
           />
           {children}
-          {/* <Card className="content__right"> */}
-          {/* <CardHeader>
-              <CardTitle>Les classes disponibles</CardTitle>
-              <CardDescription>Les classes disponibles</CardDescription>
-            </CardHeader> */}
-
-          {/* <CardContent className="right__content-container">
-              <VerticalFieldSelect
-                className="right__content"
-                placeholder="Sélectionner une classe"
-                onValueChange={(value) => console.log(value)}
-                label={rightSide.title}
-              >
-                <SelectGroup>
-                  <SelectLabel>BTS</SelectLabel>
-                  <SelectItem value="class-1ereA">
-                    1ère A - Sciences de l'Ingénieur
-                  </SelectItem>
-                </SelectGroup>
-              </VerticalFieldSelect>
-            </CardContent>
-          </Card> */}
         </CardContent>
         <CardFooter className="cards-container__footer">
           <IconArrowRightDashed
-            onClick={(e) => props.handleOnClick({ e, clickProps })}
+            {...props.functions}
+            onClick={(e) => props.onClick({ e, ...clickProps })}
             data-name="next-step"
           />
         </CardFooter>
