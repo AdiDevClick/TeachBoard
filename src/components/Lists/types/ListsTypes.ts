13f7accf65183936b740ccaf8ddfc58bec22ab8f
Adrien <adrienquijo@gmail.com>
2025-11-10 00:07:28 +0100

    Les types pour le <ListMapper/> sont maintenant mis à jour pour accepter un objet - Le mappeur permettant maintenant de transformer automatiquement un objet en array, il devrait plus remonter une erreur de type

--- src/components/Lists/types/ListsTypes.ts
+++ src/components/Lists/types/ListsTypes.ts
@@ -1,10 +1,14 @@
 import type { ReactElement, ReactNode } from "react";
 
+/** Props for the ListMapper component */
 export type ListMapperProps<T> = {
-  items: T[];
+  items: T[] | { [key: string]: T };
   children:
     | ReactElement<Partial<{ item: T; index: number }>>
-    | ((item: T, index: number) => ReactNode);
+    | ((
+        item: T extends Record<string, unknown> ? [string, T] : T,
+        index: number
+      ) => ReactNode);
 };
 
 export type ListMapperType<T> =
