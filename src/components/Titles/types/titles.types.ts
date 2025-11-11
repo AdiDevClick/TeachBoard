d53bf92858c67908823516c4f61f40c5d482c8e4
Adrien <adrienquijo@gmail.com>
2025-11-12 00:29:26 +0100

    Mise à jour des types pour coller au nom du composant

--- src/components/Titles/types/titles.types.ts
+++ src/components/Titles/types/titles.types.ts
@@ -2,6 +2,6 @@ import type { CardHeader } from "@/components/ui/card.tsx";
 import type { ComponentProps } from "react";
 
 /** Props for the ModalTitle component */
-export type ModalTitleProps = ComponentProps<typeof CardHeader> & {
+export type HeaderTitleProps = ComponentProps<typeof CardHeader> & {
   className?: string;
 };
