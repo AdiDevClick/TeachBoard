92ef076a3de1ead6bb41cfd364e56c5ee0d0bfed
Adrien <adrienquijo@gmail.com>
2025-11-12 00:30:40 +0100

    Code cleanup

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,6 @@
 import { ChartAreaInteractive } from "@/components/chart-area-interactive.tsx";
 import { DataTable } from "@/components/data-table.tsx";
-import { LoginForm } from "@/components/LoginForms/LoginForm.tsx";
 import { SectionCards } from "@/components/section-cards.tsx";
-import {
-  DialogContent,
-  DialogPortal,
-  DialogTrigger,
-} from "@/components/ui/dialog.tsx";
-import { inputControllers } from "@/data/loginInputControllers.ts";
 import data from "@data/data.json";
 
 export function Home() {
@@ -15,16 +8,7 @@ export function Home() {
     <>
       {/* <SidebarInset> */}
       {/* <SiteHeader /> */}
-      <DialogTrigger>test</DialogTrigger>
-      <DialogPortal>
-        <DialogContent style={{ overflow: "hidden", padding: 0 }}>
-          {/* <DialogHeader>
-            <DialogTitle>test</DialogTitle>
-            <DialogDescription>test</DialogDescription>
-          </DialogHeader> */}
-          <LoginForm inputControllers={inputControllers} modalMode={true} />
-        </DialogContent>
-      </DialogPortal>
+      {/* </DialogPortal> */}
       <div className="flex flex-1 flex-col">
         <div className="@container/main flex flex-1 flex-col gap-2">
           <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
