9d19abbb428bc60afdfd793cd22026e048e088b0
Adrien <adrienquijo@gmail.com>
2025-11-12 00:29:51 +0100

    Création d'un fichier de config qui contient les endpoints de l'API pour plus de simplicité

---
+++ src/configs/api.endpoints.config.ts
@@ -0,0 +1,26 @@
+const BASE_API_URL = "/api";
+
+/**
+ * API Endpoints Configuration
+ */
+export const API_ENDPOINTS = {
+  GET: {
+    METHOD: "GET",
+    CLASSES: {
+      ALL: `${BASE_API_URL}/classes/`,
+      BY_ID: (id: number | string) => `${BASE_API_URL}/classes/${id}`,
+    },
+    STUDENTS: `${BASE_API_URL}/students`,
+    COURSES: `${BASE_API_URL}/courses`,
+    USERS: `${BASE_API_URL}/users`,
+    POSTS: `${BASE_API_URL}/posts`,
+  },
+  POST: {
+    METHOD: "POST",
+    CREATE_CLASS: `${BASE_API_URL}/classes/create`,
+    AUTH: {
+      LOGIN: `${BASE_API_URL}/auth/login`,
+      REGISTER: `${BASE_API_URL}/auth/register`,
+    },
+  },
+};
