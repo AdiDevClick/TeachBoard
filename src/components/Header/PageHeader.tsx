c5948e9b8a82dbdfa1982af41f791e1a6380c299
Adrien <adrienquijo@gmail.com>
2025-11-12 00:25:27 +0100

    Ajout d'un bouton permettant de se connecter directement depuis le header - Ce bouton fera appel à la modale - Lorsque la modale est ouverte, elle pourra modifier l'URL mais aussi revenir à l'ancienne -

--- src/components/Header/PageHeader.tsx
+++ src/components/Header/PageHeader.tsx
@@ -1,3 +1,4 @@
+import { LoginForm } from "@/components/LoginForms/LoginForm.tsx";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -7,21 +8,57 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb.tsx";
 import { Button } from "@/components/ui/button";
+import { DialogContent } from "@/components/ui/dialog.tsx";
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { inputControllers } from "@/data/loginInputControllers.ts";
+import { useDialog } from "@/hooks/contexts/useDialog.ts";
 import "@css/PageHeader.scss";
-import { Link, useLocation } from "react-router-dom";
+import { useEffect, useRef, type MouseEvent } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 /** Page header component
  *
  * @description This component renders the page header including breadcrumb navigation and action buttons.
  */
 export function PageHeader() {
-  // const { pathname } = useLocation();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { isDialogOpen, openDialog, closeDialog } = useDialog();
+  const ref = useRef<HTMLDivElement>(null);
 
-  // if (pathname === "/login" || pathname === "/register") {
-  //   return null;
-  // }
+  useEffect(() => {
+    const handlePopState = (e: PopStateEvent) => {
+      e.preventDefault();
+      closeDialog();
+    };
+
+    window.addEventListener("popstate", handlePopState);
+
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    if (isDialogOpen) {
+      console.log("Dialog open state changed:", isDialogOpen);
+    } else if (location.state?.background) {
+      navigate(location.state.background, { replace: true, state: {} });
+    }
+  }, [isDialogOpen, location.state]);
+
+  const handleLoginClick = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    location.state = { background: location.pathname };
+    history.pushState(location.state, "", "/login");
+
+    openDialog();
+  };
 
   return (
     <header className="page__header-container">
@@ -56,6 +93,21 @@ export function PageHeader() {
               GitHub
             </Link>
           </Button>
+          <Button
+            variant="ghost"
+            size="sm"
+            className="actions__button dark:text-foreground"
+            onClick={handleLoginClick}
+          >
+            Se connecter
+          </Button>
+          <DialogContent style={{ overflow: "hidden", padding: 0 }}>
+            <LoginForm
+              ref={ref}
+              inputControllers={inputControllers}
+              modalMode={true}
+            />
+          </DialogContent>
         </div>
       </div>
     </header>
