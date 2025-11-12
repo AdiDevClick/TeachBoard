0532b98db45bee4be01b9dd112a459e36bc11cbf
Adrien <adrienquijo@gmail.com>
2025-11-12 11:21:00 +0100

    code cleanup

--- src/api/contexts/DialogContext.ts
+++ src/api/contexts/DialogContext.ts
@@ -1,16 +1,4 @@
-import type { MouseEvent as ReactMouseEvent } from "react";
 import { createContext } from "react";
-
-export type DialogState = {
-  isDialogOpen: boolean;
-  container: HTMLDivElement | null;
-};
-
-export type DialogTriggerHandler = (
-  event: ReactMouseEvent<HTMLElement>
-) => void;
-
-export type DialogOpenChangeHandler = (open: boolean) => void;
 
 export type DialogContextType = {
   isDialogOpen: boolean;
