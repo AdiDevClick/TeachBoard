7fc66a0153250bbf30de56dcb726e7a7d9dced02
Adrien <adrienquijo@gmail.com>
2025-11-13 19:10:47 +0100

    Le mappeur étant plus stable, un shinanigan de types n'est plus nécessaire - Code cleanup

--- src/components/Buttons/types/ButtonTypes.ts
+++ src/components/Buttons/types/ButtonTypes.ts
@@ -1,27 +1,7 @@
 import type { LoginButtonsSvgsType } from "@/configs/social.config";
-import type { ExcludeProps } from "@/utils/types/types.utils.ts";
 import type { ComponentProps } from "react";
 
 /**
  * LOGIN BUTTON types
  */
-
-/** Base props common to both modes */
-type LoginButtonBaseProps = {
-  index: number;
-};
-
-/** Props when used standalone - all LoginButtonsSvgsType props are required, __mapped is forbidden */
-type LoginButtonStandaloneProps = LoginButtonBaseProps &
-  LoginButtonsSvgsType & {
-    __mapped?: false;
-  };
-
-/** Props when used with ListMapper - LoginButtonsSvgsType props are optional, __mapped is required */
-type LoginButtonMappedProps = ExcludeProps<LoginButtonsSvgsType> &
-  ExcludeProps<LoginButtonBaseProps> & {
-    __mapped?: true;
-  };
-
-export type LoginButtonProps = ComponentProps<"button"> &
-  (LoginButtonMappedProps | LoginButtonStandaloneProps);
+export type LoginButtonProps = ComponentProps<"button"> & LoginButtonsSvgsType;
