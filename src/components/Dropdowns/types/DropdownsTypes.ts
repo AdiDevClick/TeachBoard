f1953a8e5e3ddb9f942ecad574852457e456690b
Adrien <adrienquijo@gmail.com>
2025-11-15 18:21:24 +0100

    Le dropdown peut maintenant être utilisé en standalone facilement - Suppression de la variable isUserConnected car il devra automatiquemetn se mettre à jour avec les paramètres de triggeredSettings

--- src/components/Dropdowns/types/DropdownsTypes.ts
+++ src/components/Dropdowns/types/DropdownsTypes.ts
@@ -1,5 +1,6 @@
 import type { TriggeredSettingsPopup } from "@/components/Sidebar/footer/settings_popup/TriggeredSettingsPopup.tsx";
 import type { sidebarDatas } from "@/data/SidebarData.ts";
+import type { SafeListMapperProp } from "@/utils/types/types.utils.ts";
 import type { ComponentProps } from "react";
 
 /** Props for each item in the list */
@@ -9,13 +10,19 @@ type DropdownItem = (typeof sidebarDatas.user.settings)[number];
 type DropdownUserData = Omit<
   Parameters<typeof TriggeredSettingsPopup>[0]["userData"],
   "settings"
-> & {
-  isUserConnected?: boolean;
-};
+>;
+// type DropdownUserData = Omit<
+//   Parameters<typeof TriggeredSettingsPopup>[0]["userData"],
+//   "settings"
+// > & {
+//   isUserConnected?: boolean;
+// };
 
 /**
  * @description The items do not inherently need user data, but it might not be displayed to the user if they are not connected.
  */
 export type DropdownsProps = ComponentProps<"div"> &
-  DropdownItem &
-  DropdownUserData;
+  (
+    | (DropdownItem & DropdownUserData)
+    | SafeListMapperProp<DropdownItem & DropdownUserData>
+  );
