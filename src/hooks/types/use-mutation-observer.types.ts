7ced20e30d47fd128b48516618dd3e295bf40492
Adrien <adrienquijo@gmail.com>
2025-11-18 00:42:25 +0100

    Ajout d'un hook permettant d'avoir un contrôle sur une ref et l'élément qu'elle contient - Le hook est un observer - Le node ainsi que la ref créée peuvent être récupérés à tout moment - Utile pour trigger un callback ou s'assurer qu'une ref est bien dispo

---
+++ src/hooks/types/use-mutation-observer.types.ts
@@ -0,0 +1,19 @@
+/**
+ * Mutation Observer Options
+ */
+export type MutationObserverOptions = {
+  /** Indique si l'on observe les attributs. */
+  attributes?: boolean;
+  /** Indique si l'on observe les modifications du contenu textuel. */
+  characterData?: boolean;
+  /** Indique si l'on observe l'ajout/suppression de nœuds enfants. */
+  childList?: boolean;
+  /** Indique si l'observation concerne aussi les descendants. */
+  subtree?: boolean;
+};
+
+export interface MutationObserverHook {
+  callback?: MutationCallback;
+  options?: MutationObserverOptions;
+  onNodeReady?: (node: HTMLElement) => void;
+}
