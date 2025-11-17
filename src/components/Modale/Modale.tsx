76cfe5d2d3eb1ed346693b624a602c1ce93a2848
Adrien <adrienquijo@gmail.com>
2025-11-18 00:39:21 +0100

    La modale a reçu un gros changement - Quand un élément est fermé, il peut maintenant changer d'url automatiquement et revenu à son état précédent dès qu'elle sera fermée - Le staté n'était pas obligatoire mais permet de s'assurer que les éléments HTML soient déjà chargés -

--- src/components/Modale/Modale.tsx
+++ src/components/Modale/Modale.tsx
@@ -1,7 +1,10 @@
 import type { ModaleProps } from "@/components/Modale/types/modale.types.ts";
 import { Dialog, DialogContent } from "@/components/ui/dialog.tsx";
 import { useDialog } from "@/hooks/contexts/useDialog.ts";
+import { wait } from "@/utils/utils.ts";
 import "@css/Dialog.scss";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 /**
  * Modale component
@@ -20,15 +23,102 @@ export function Modale({
   children,
   modaleContent,
   modaleName,
+  onNodeReady,
+  onOpen,
   onOpenChange,
 }: Readonly<ModaleProps>) {
+  const location = useLocation();
+  const navigate = useNavigate();
   const { isDialogOpen, onOpenChange: contextOnOpenChange } = useDialog();
-  const className = `dialog__content--${modaleName}`;
+
+  const [modalState, setModalState] = useState({
+    forward: null! as boolean,
+    isReady: false as HTMLElement | false,
+    previousUrl: "",
+    modaleName: "",
+    isOpen: false,
+    locationState: "",
+    url: "",
+  });
+
+  const className = `dialog__content dialog__content--${modaleName}`;
   const onOpenChangeHandler = onOpenChange ?? contextOnOpenChange;
+
+  const isOpen = onOpen ?? isDialogOpen(modaleName);
+  const newUrl = `/${modaleName}`;
+
+  /**
+   * URL navigation effect
+   */
+  useEffect(() => {
+    if (modalState.forward === null) return;
+
+    if (modalState.forward) {
+      const waitAndPush = async () => {
+        await wait(100);
+        history.pushState(modalState.previousUrl, "", newUrl);
+      };
+      waitAndPush();
+    } else {
+      navigate(modalState.previousUrl, {
+        replace: true,
+        state: {},
+      });
+    }
+  }, [modalState.forward]);
+
+  /**
+   * Verify and set the onNodeReady element
+   */
+  useEffect(() => {
+    if (onNodeReady && modalState.isReady !== onNodeReady) {
+      setModalState((prev) => ({
+        ...prev,
+        isReady: onNodeReady ?? false,
+      }));
+    }
+  }, [onNodeReady, modalState.isReady]);
+
+  /**
+   * Open/close state changes
+   */
+  useEffect(() => {
+    if (!modalState.isReady) return;
+
+    let stateObject = null;
+    let forward = null;
+
+    if (isOpen && !modalState.isOpen && location.pathname !== newUrl) {
+      forward = true;
+      stateObject = {
+        isOpen: true,
+        modaleName,
+        previousUrl: location.pathname,
+        url: newUrl,
+      };
+    }
+
+    if (!isOpen && modalState.isOpen && modalState.previousUrl) {
+      forward = false;
+      stateObject = {
+        isOpen: false,
+        previousUrl: "",
+        url: "",
+      };
+    }
+
+    if (forward === null) return;
+
+    setModalState((prev) => ({
+      ...prev,
+      ...stateObject,
+      forward,
+    }));
+  }, [isOpen, location.pathname, modaleName, modalState.isReady]);
 
   return (
     <Dialog
-      open={isDialogOpen(modaleName)}
+      open={isOpen as boolean}
       onOpenChange={() => onOpenChangeHandler(modaleName)}
     >
       {children}
