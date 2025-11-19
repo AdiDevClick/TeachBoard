48056f8fd79eea8c12902e670e05c7f4585b51ed
Adrien <adrienquijo@gmail.com>
2025-11-19 17:03:07 +0100

    Mise à jour des types

--- src/api/contexts/types/context.types.ts
+++ src/api/contexts/types/context.types.ts
@@ -1,12 +1,16 @@
 import type { AppModalNames } from "@/configs/app.config.ts";
+import type { PreventDefaultAndStopPropagation } from "@/utils/types/types.utils.ts";
 
 /**
  * Type for Dialog context
  */
 export type DialogContextType = {
   isDialogOpen: (id: AppModalNames) => boolean;
-  openDialog: (id: AppModalNames) => void;
-  closeDialog: (id?: AppModalNames) => void;
+  openDialog: (e: PreventDefaultAndStopPropagation, id: AppModalNames) => void;
+  closeDialog: (
+    e: PreventDefaultAndStopPropagation,
+    id?: AppModalNames
+  ) => void;
   onOpenChange: (id: AppModalNames) => void;
   closeAllDialogs: () => void;
 };
