86e1e863b0dd8b87796fccc81dec478e30e65854
Adrien <adrienquijo@gmail.com>
2025-11-23 19:33:37 +0100

    Ajout du schéma pour l'email de recovery

--- src/components/LoginForms/types/login-forms.types.ts
+++ src/components/LoginForms/types/login-forms.types.ts
@@ -1,9 +1,12 @@
 import type { InputItem } from "@/components/Inputs/types/InputsTypes.ts";
 import { formSchema } from "@/models/login.models.ts";
+import type { pwRecoverySchema } from "@/models/pw-recovery.model.ts";
 import type { ComponentProps } from "react";
 import { z } from "zod";
 
 export type LoginFormSchema = z.infer<typeof formSchema>;
+
+export type RecoveryFormSchema = z.infer<typeof pwRecoverySchema>;
 
 export type LoginInputItem = InputItem<LoginFormSchema>;
 
