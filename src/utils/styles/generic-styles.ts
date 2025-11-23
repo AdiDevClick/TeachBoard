31d75214bb9e031d37b7ee39052da39200ff13c4
Adrien <adrienquijo@gmail.com>
2025-11-23 22:42:44 +0100

    genericStyle exporte directement ses variables pour éviter de les réinstancier partout

--- src/utils/styles/generic-styles.ts
+++ src/utils/styles/generic-styles.ts
@@ -3,7 +3,7 @@
  *
  * @returns An object containing class names for the generic page elements.
  */
-export function genericStyle() {
+function genericStyle() {
   const genericPage = "generic-page";
 
   return {
@@ -14,3 +14,19 @@ export function genericStyle() {
     logoIconStyle: { className: `${genericPage}__logo--icon` },
   };
 }
+
+/**
+ * Generic styles for the application
+ *
+ * @description These styles can be imported and used across different components and pages
+ * to maintain a consistent look and feel.
+ *
+ * {@link #genericStyle()}
+ */
+export const {
+  containerStyle: GENERIC_CONTAINER_STYLE,
+  contentStyle: GENERIC_CONTENT_STYLE,
+  logoStyle: GENERIC_LOGO_STYLE,
+  logoBackgroundStyle: GENERIC_LOGO_BACKGROUND_STYLE,
+  logoIconStyle: GENERIC_LOGO_ICON_STYLE,
+} = genericStyle();
