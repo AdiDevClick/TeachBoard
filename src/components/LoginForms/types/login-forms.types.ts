649b9f7173c4bc9aa991729ef247408680e51acf
Adrien <adrienquijo@gmail.com>
2025-11-24 13:45:08 +0100

    Code cleanup

--- src/components/LoginForms/types/login-forms.types.ts
+++ src/components/LoginForms/types/login-forms.types.ts
@@ -1,7 +1,6 @@
 import type { InputItem } from "@/components/Inputs/types/InputsTypes.ts";
 import { formSchema } from "@/models/login.models.ts";
 import type { pwRecoverySchema } from "@/models/pw-recovery.model.ts";
-import type { ComponentProps } from "react";
 import { z } from "zod";
 
 export type LoginFormSchema = z.infer<typeof formSchema>;
@@ -9,8 +8,3 @@ export type LoginFormSchema = z.infer<typeof formSchema>;
 export type RecoveryFormSchema = z.infer<typeof pwRecoverySchema>;
 
 export type LoginInputItem = InputItem<LoginFormSchema>;
-
-export interface LoginFormProps extends ComponentProps<"div"> {
-  inputControllers: LoginInputItem[];
-  modaleMode?: boolean;
-}
