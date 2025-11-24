4e5460667b221f7b0336caac024d8268be6b8b5d
Adrien <adrienquijo@gmail.com>
2025-11-24 14:05:28 +0100

    Migration des types pour la page Signup

--- src/pages/Signup/types/signup.types.ts
+++ src/pages/Signup/types/signup.types.ts
@@ -1,4 +1,11 @@
+import type { InputItem } from "@/components/Inputs/types/InputsTypes.ts";
+import type { signupSchema } from "@/models/signup.models.ts";
 import type { HTMLAttributes } from "react";
+import type z from "zod";
+
+export type SignupFormSchema = z.infer<typeof signupSchema>;
+
+export type SignupInputItem = InputItem<SignupFormSchema>;
 
 export type EmailValidationProps = {
   className?: string;
