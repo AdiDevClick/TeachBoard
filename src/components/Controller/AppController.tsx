4b5152984627f8aab21bbfd049ba88b95f368882
Adrien <adrienquijo@gmail.com>
2025-11-28 21:14:08 +0100

    Meilleur typage du controller pour s'adapter à plus de situations

--- src/components/Controller/AppController.tsx
+++ src/components/Controller/AppController.tsx
@@ -1,36 +1,8 @@
 import type { WrapperProps } from "@/components/Controller/types/controller.types.ts";
 import { Field, FieldError } from "@/components/ui/field.tsx";
-import type {
-  AnyComponentLike,
-  ComponentPropsOf,
-} from "@/utils/types/types.utils.ts";
+import type { AnyComponentLike } from "@/utils/types/types.utils.ts";
+import type { ComponentProps, JSX } from "react";
 import { Controller, type FieldValues } from "react-hook-form";
-
-/**
- * ControlledInputs component for rendering a list of input fields.
- *
- * @param items - Array of input controller objects.
- * @param form - React Hook Form instance for managing form state.
- */
-// export function AppController<T extends FieldValues>({
-//   ref,
-//   name,
-//   form,
-//   ...props
-// }: Readonly<AppController<T>>) {
-//   return (
-//     <Controller
-//       name={name}
-//       control={form.control}
-//       render={({ field, fieldState }) => (
-//         <Field ref={ref} {...props} data-invalid={fieldState.invalid}>
-//           {props.children}
-//           <FieldError errors={[fieldState.error]} />
-//         </Field>
-//       )}
-//     />
-//   );
-// }
 
 /**
  * Wrap a component with react-hook-form Controller.
@@ -38,9 +10,15 @@ import { Controller, type FieldValues } from "react-hook-form";
  * @param Wrapped - The component to be wrapped with the Controller.
  */
 function withController<C extends AnyComponentLike>(Wrapped: C) {
-  return function Component<T extends FieldValues>(props: WrapperProps<T, C>) {
-    const { ref, name, form, ...restProps } = props;
-
+  return function Component<T extends FieldValues>({
+    ref,
+    ...props
+  }: WrapperProps<T, C>) {
+    const typedProps = props as WrapperProps<T, C>;
+    if (!typedProps?.form || !typedProps?.name) {
+      return null;
+    }
+    const { name, form, ...restProps } = typedProps;
     return (
       <Controller
         name={name}
@@ -48,9 +26,11 @@ function withController<C extends AnyComponentLike>(Wrapped: C) {
         render={({ field, fieldState }) => (
           <Field ref={ref} data-invalid={fieldState.invalid}>
             <Wrapped
-              {...(restProps as ComponentPropsOf<AnyComponentLike>)}
-              field={field}
-              fieldState={fieldState}
+              {...({
+                ...restProps,
+                field,
+                fieldState,
+              } as ComponentProps<C>)}
             />
             <FieldError errors={[fieldState.error]} />
           </Field>
