6eb1f0dc8ae282ed235055c4bfd660c32f270928
Adrien <adrienquijo@gmail.com>
2025-11-28 20:59:00 +0100

    Ajout de HOC pour le VerticalField - Il est maintenant possible d'avoir des versions avec controller, commands ou bien un listmapper  - Cela permettra d'éviter certains fichiers inutiles Il reste encore du typage à faire -

--- src/components/Selects/VerticalFieldSelect.tsx
+++ src/components/Selects/VerticalFieldSelect.tsx
@@ -1,4 +1,14 @@
+import { WithController } from "@/components/Controller/AppController.tsx";
+import { WithListMapper } from "@/components/Lists/ListMapper.tsx";
+import { SelectItemWithIcon } from "@/components/Selects/select-item-with-icon/SelectItemWithIcon.tsx";
 import type { VerticalSelectProps } from "@/components/Selects/types/select.types.ts";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandList,
+} from "@/components/ui/command.tsx";
 import { Label } from "@/components/ui/label";
 import {
   Select,
@@ -6,8 +16,9 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { cn } from "@/utils/utils";
-import { useId } from "react";
+import type { AnyComponentLike } from "@/utils/types/types.utils.ts";
+import { cn, preventDefaultAndStopPropagation } from "@/utils/utils";
+import { useId, type PointerEvent, type ReactNode, type Ref } from "react";
 
 /**
  * A card-like select field with vertical layout on selection options.
@@ -25,31 +36,118 @@ export function VerticalFieldSelect({
   fullWidth = true,
   className,
   side = "bottom",
+  setRef,
+  id: containerId,
   ...props
 }: VerticalSelectProps) {
   const id = useId();
 
+  const { onOpenChange, onValueChange, children, defaultValue, ...rest } =
+    props;
+
   return (
-    <div className={cn("flex flex-col items-start gap-2", className)}>
+    <div
+      id={containerId}
+      ref={setRef as Ref<HTMLDivElement>}
+      data-inputcontrollername={rest.field?.name}
+      className={cn("flex flex-col items-start gap-2", className)}
+    >
       {label && (
         <Label className="w-full" htmlFor={id}>
           {label}
         </Label>
       )}
 
-      <Select {...props}>
+      <Select
+        defaultValue={defaultValue}
+        onOpenChange={onOpenChange}
+        onValueChange={onValueChange}
+      >
         <SelectTrigger
           id={id}
+          {...rest}
           className={cn(fullWidth ? "w-full" : "w-fit")}
           size="default"
         >
           <SelectValue placeholder={placeholder} />
         </SelectTrigger>
 
-        <SelectContent side={side}>{props.children}</SelectContent>
+        <SelectContent side={side}>{children}</SelectContent>
       </Select>
     </div>
   );
 }
 
+function withCommands(Wrapped: AnyComponentLike) {
+  type CommandsProps = {
+    useCommands?: boolean;
+    creationButtonText?: ReactNode;
+    task: string;
+    apiEndpoint?: string;
+    useButtonAddNew?: boolean;
+    onAddNewItem?: (e: PointerEvent<HTMLDivElement>) => void;
+  };
+
+  return function Component<T extends CommandsProps>(
+    props: T & VerticalSelectProps
+  ) {
+    const {
+      useCommands,
+      children,
+      creationButtonText,
+      task,
+      apiEndpoint,
+      useButtonAddNew,
+      onAddNewItem,
+      ...rest
+    } = props;
+
+    const handleAddNew = async (
+      e: PointerEvent<HTMLDivElement>,
+      task: string | undefined,
+      apiEndpoint: string | undefined
+    ) => {
+      preventDefaultAndStopPropagation(e);
+      if (!task || !apiEndpoint) return;
+      console.log(e.target, task, apiEndpoint);
+    };
+
+    return (
+      <Wrapped {...rest}>
+        {useCommands && (
+          <Command>
+            <CommandInput placeholder="Search..." />
+            <CommandList>
+              <CommandEmpty>Aucun résultat trouvé.</CommandEmpty>
+              <CommandGroup></CommandGroup>
+            </CommandList>
+          </Command>
+        )}
+        {useButtonAddNew && (
+          <SelectItemWithIcon
+            value={task}
+            selectText={creationButtonText}
+            onPointerDown={onAddNewItem}
+          />
+        )}
+        {children}
+      </Wrapped>
+    );
+  };
+}
+
 export default VerticalFieldSelect;
+
+export const VerticalFieldSelectWithController =
+  WithController(VerticalFieldSelect);
+
+export const VerticalFieldSelectWithCommands =
+  withCommands(VerticalFieldSelect);
+
+export const VerticalFieldSelectWithControlledCommands = WithController(
+  VerticalFieldSelectWithCommands
+);
+
+export const VerticalFieldSelectWithControllerAndCommandsList: ReturnType<
+  typeof WithListMapper<typeof VerticalFieldSelectWithControlledCommands>
+> = WithListMapper(VerticalFieldSelectWithControlledCommands);
