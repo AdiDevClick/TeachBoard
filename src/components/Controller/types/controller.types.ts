9b2e41abcad84c4af89823117ec2f1c98793ec93
Adrien <adrienquijo@gmail.com>
2025-11-28 21:16:40 +0100

    Création d'un fichier pour les types du controlleur

---
+++ src/components/Controller/types/controller.types.ts
@@ -0,0 +1,32 @@
+import type {
+  AnyComponentLike,
+  ComponentPropsOf,
+} from "@/utils/types/types.utils.ts";
+import type { ComponentProps, ComponentPropsWithRef } from "react";
+import type { FieldValues, Path, UseFormReturn } from "react-hook-form";
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export type AppControllerProps<T extends FieldValues> = {
+  name: Path<T>;
+  form: UseFormReturn<T> | UseFormReturn<any>;
+};
+
+/**
+ * Wrapper props for a controlled component.
+ * It provides the `name` and `form` props while forwarding the
+ * wrapped component's props, excluding injected props like `field` and `fieldState`.
+ */
+export type WrapperProps<T extends FieldValues, C> = AppControllerProps<T> &
+  Omit<ComponentPropsOf<C>, "field" | "fieldState" | "ref"> &
+  ComponentPropsWithRef<"div">;
+
+/**
+ * Same as WrapperProps but with a non-generic form (any),
+ * useful for HOC Users where they shouldn't need to specify `T`.
+ */
+
+export type WrapperPropsAny<C extends AnyComponentLike = AnyComponentLike> =
+  // intentionally accept any form instance type to reduce HOC friction in JSX
+  AppControllerProps<any> &
+    Omit<ComponentProps<C>, "field" | "fieldState" | "ref"> &
+    ComponentPropsWithRef<"div">;
