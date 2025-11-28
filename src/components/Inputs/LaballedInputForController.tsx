5579d9e43d14096d746023733f56efc284ccf8ed
Adrien <adrienquijo@gmail.com>
2025-11-28 21:02:54 +0100

    Ajout d'un nouveau composant qui a des HOC pouvant être utilisé avec le list mapper et un controller - Mise à jour du fichier de types pour coller à ce qui a été fait jusqu'à présent

---
+++ src/components/Inputs/LaballedInputForController.tsx
@@ -0,0 +1,41 @@
+import { WithController } from "@/components/Controller/AppController.tsx";
+import type { LaballedInputForControllerProps } from "@/components/Inputs/types/inputs.types";
+import { WithListMapper } from "@/components/Lists/ListMapper.tsx";
+import { Input } from "@/components/ui/input.tsx";
+import { Label } from "@/components/ui/label.tsx";
+import type { FieldValues } from "react-hook-form";
+
+/**
+ * A labelled input component integrated with react-hook-form Controller.
+ *
+ * @param props - Props for the labelled input component
+ * @returns
+ */
+export function LaballedInputForController<T extends FieldValues>(
+  props: LaballedInputForControllerProps<T>
+) {
+  if (!("field" in props) || !("fieldState" in props)) {
+    return null;
+  }
+
+  const { name, title, field, fieldState, ...rest } = props;
+
+  return (
+    <>
+      <Label htmlFor={name ?? field.name}>{title}</Label>
+      <Input
+        {...rest}
+        {...field}
+        id={name ?? field.name ?? "intput-is-not-named"}
+        aria-invalid={fieldState.invalid ?? false}
+        required
+      />
+    </>
+  );
+}
+
+export const ControlledLabelledInput = WithController(
+  LaballedInputForController
+);
+
+export const ControlledInputList = WithListMapper(ControlledLabelledInput);
