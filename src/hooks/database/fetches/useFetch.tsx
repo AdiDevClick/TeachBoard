4c51f6a7b04683dde4d3d1915821226bfc957e7f
Adrien <adrienquijo@gmail.com>
2025-11-30 20:42:08 +0100

    Petite mise à jour qui dessine la future utilisation du hook pour des fetch GET

--- src/hooks/database/fetches/useFetch.tsx
+++ src/hooks/database/fetches/useFetch.tsx
@@ -1,16 +1,47 @@
 import { API_ENDPOINTS } from "@/configs/api.endpoints.config.ts";
+import { USER_ACTIVITIES } from "@/configs/app.config.ts";
 import { useQueryOnSubmit } from "@/hooks/database/useQueryOnSubmit.ts";
+import { useState } from "react";
+type FetchParams = {
+  contentId: (typeof USER_ACTIVITIES)[keyof typeof USER_ACTIVITIES];
+  page: number;
+  pageSize: number;
+  filters: Record<string, unknown>;
+  sortBy: string;
+  sortOrder: "asc" | "desc";
+  url: string;
+  headers?: Headers;
+};
+const defaultStateParameters: FetchParams = {
+  contentId: USER_ACTIVITIES.none,
+  page: 1,
+  pageSize: 10,
+  filters: {},
+  sortBy: "",
+  sortOrder: "asc",
+  url: "",
+  headers: undefined,
+};
 
-export function useFetch(url, contentId) {
-  return useQueryOnSubmit([
-    contentId,
+export function useFetch() {
+  const [fetchParams, setFetchParams] = useState(defaultStateParameters);
+
+  const queryParams = useQueryOnSubmit([
+    fetchParams.contentId,
     {
       method: API_ENDPOINTS.GET.METHOD,
-      url: url,
+      url: fetchParams.url,
+      headers: fetchParams.headers,
       // silent: true,
       // onSuccess: (response) => {
       //   return response;
       // },
     },
   ]);
+
+  return {
+    fetchParams,
+    setFetchParams,
+    ...queryParams,
+  };
 }
