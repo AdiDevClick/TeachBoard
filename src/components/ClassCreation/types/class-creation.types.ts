8603544066046fa0cfb4b9d4ef4e8b541810460a
Adrien <adrienquijo@gmail.com>
2025-12-02 00:14:35 +0100

    code cleanup

--- src/components/ClassCreation/types/class-creation.types.ts
+++ src/components/ClassCreation/types/class-creation.types.ts
@@ -1,7 +1,1 @@
-import type { InputItem } from "@/components/Inputs/types/inputs.types";
-import type { classCreationSchema } from "@/models/class-creation.models.ts";
-import type z from "zod";
 
-export type ClassCreationFormSchema = z.infer<typeof classCreationSchema>;
-
-export type ClassCreationInputItem = InputItem<ClassCreationFormSchema>;
