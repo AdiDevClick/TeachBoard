249b3bd1045861215222e1503fdbc1213d72d3a2
Adrien <adrienquijo@gmail.com>
2025-12-06 12:36:06 +0100

    Petite mise à jour des types pour les props de degreeskill

--- src/components/ClassCreation/diploma/degree-skill/types/degree-skill.types.ts
+++ src/components/ClassCreation/diploma/degree-skill/types/degree-skill.types.ts
@@ -1,16 +1,13 @@
+import type DegreeSkill from "@/components/ClassCreation/diploma/degree-skill/DegreeSkill.tsx";
 import type { useDegreeCreationForm } from "@/hooks/database/classes/useDegreeCreationForm.ts";
-import type {
-  DegreeCreationFormSchema,
-  DegreeCreationInputItem,
-} from "@/models/degree-creation.models.ts";
+import type { useFetch } from "@/hooks/database/fetches/useFetch.tsx";
+import type { DegreeSkillFormSchema } from "@/models/degree-skill.models.ts";
 import type { FieldValues, useForm } from "react-hook-form";
 
-export type DegreeSkillProps<T extends FieldValues = DegreeCreationFormSchema> =
-  {
-    formId?: string;
-    form: ReturnType<typeof useForm<T>>;
-    inputControllers?: DegreeCreationInputItem[];
-    pageId?: string;
-    className?: string;
-    queryHooks: ReturnType<typeof useDegreeCreationForm>;
-  };
+export type DegreeSkillProps<T extends FieldValues = DegreeSkillFormSchema> = {
+  formId?: string;
+  form: ReturnType<typeof useForm<T>>;
+  className?: string;
+  queryHooks: ReturnType<typeof useDegreeCreationForm>;
+  fetchHooks: ReturnType<typeof useFetch>;
+} & Omit<Parameters<typeof DegreeSkill>[0], "modalMode">;
