3978726e3a0f3d1a3260d055ed3ea4ed1cddeda9
Adrien <adrienquijo@gmail.com>
2025-12-10 00:27:44 +0100

    Ce composant de vue centralise le form resolver ainsique le useFetch pour le re render -

---
+++ src/components/ClassCreation/diploma/degree-module-skill/DegreeModuleSkill.tsx
@@ -0,0 +1,73 @@
+import { DegreeModuleSkillController } from "@/components/ClassCreation/diploma/degree-module-skill/controller/DegreeModuleSkillController.tsx";
+import withTitledCard from "@/components/HOCs/withTitledCard.tsx";
+import { degreeSubSkillsCreationInputControllers } from "@/data/inputs-controllers.data.ts";
+import { useFetch } from "@/hooks/database/fetches/useFetch.tsx";
+import type {
+  DegreeModuleSkillFormSchema,
+  DegreeModuleSkillInputItem,
+} from "@/models/degree-module-skill.model.ts";
+import moduleSkillSchema from "@/models/degree-module-skill.model.ts";
+import type { PageWithControllers } from "@/types/AppPagesInterface.ts";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMemo } from "react";
+import { useForm } from "react-hook-form";
+
+const titleProps = {
+  title: "Création de nouvelles compétences",
+  description:
+    "Ces compétences pourront être associées aux modules de diplôme.",
+};
+
+const footerProps = { submitText: "Ajouter", cancelText: "Annuler" };
+
+/**
+ * View component for creating a new degree skill.
+ *
+ * @description This inits Zod validated form
+ * @param pageId - The ID of the page.
+ * @param formId - The ID of the form.
+ * @param inputControllers - The input controllers for the form (this needs to be already validated by Zod Schema).
+ * @param modalMode - Whether the component is in modal mode.
+ * @param props - Additional props.
+ */
+export function DegreeModuleSkill({
+  pageId = "new-degree-module-skill",
+  modalMode = true,
+  inputControllers = degreeSubSkillsCreationInputControllers,
+  ...props
+}: Readonly<PageWithControllers<DegreeModuleSkillInputItem>>) {
+  const formId = pageId + "-form";
+  const fetchHooks = useFetch();
+  const form = useForm<DegreeModuleSkillFormSchema>({
+    resolver: zodResolver(moduleSkillSchema),
+    mode: "onTouched",
+    defaultValues: {
+      name: "",
+      code: "",
+    },
+  });
+  const commonProps = useMemo(
+    () => ({
+      pageId,
+      inputControllers,
+      formId,
+      form,
+      modalMode,
+      footerProps: {
+        ...footerProps,
+        formState: form.formState,
+        formId,
+      },
+      titleProps,
+      fetchHooks,
+      ...props,
+    }),
+    [form.formState, props]
+  );
+
+  return <DegreeModuleSkillWithCard {...commonProps} />;
+}
+
+const DegreeModuleSkillWithCard = withTitledCard(DegreeModuleSkillController);
+
+export default DegreeModuleSkill;
