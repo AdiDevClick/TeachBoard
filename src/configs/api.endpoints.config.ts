9644e2275ed4d8fed18ace6944f946c500d8dbf5
Adrien <adrienquijo@gmail.com>
2025-12-16 17:20:22 +0100

    Le reshape de données du GET TASKSTEMPLATES est désormais correcte - Le nom de la task est display mais l'id correspond à la task template -

--- src/configs/api.endpoints.config.ts
+++ src/configs/api.endpoints.config.ts
@@ -86,7 +86,11 @@ export const API_ENDPOINTS = Object.freeze({
           `${BASE_API_URL}/task-templates/by-degree-config/${id}`,
       },
       dataReshape: (data: any) =>
-        dataReshaper(data).rename("tasks", "items").newShape(),
+        dataReshaper(data)
+          .selectElementsTo(["task", "id"], "items")
+          .add({ groupTitle: "Tous" })
+          .assign([["name", "value"]])
+          .newShape(),
     },
     TASKS: {
       endpoint: `${BASE_API_URL}/tasks`,
@@ -163,8 +167,9 @@ export const API_ENDPOINTS = Object.freeze({
       endpoint: `${BASE_API_URL}/task-templates`,
       dataReshape: (data: any, cachedDatas: unknown) => {
         const newItem = {
-          ...data.taskTemplate,
-          value: data.taskTemplate.name,
+          ...data,
+          value: data.task.name,
+          // value: data.name,
         };
         return reshapeItemToCachedData(newItem, cachedDatas, "Tous");
       },
@@ -215,3 +220,75 @@ function reshapeItemToCachedData(
     .addTo(newItem, "items", "groupTitle", groupConditionValue)
     .newShape();
 }
+
+const dataCreationTaskTemplateSaved = {
+  id: "1f0da0a0-364c-6bfd-9bea-b538946f0c8f",
+  name: "testsdfdsfdsfdszz",
+  task: {
+    id: "1f0d7fda-7fe4-6d11-9092-4b33a18e1890",
+    name: "Test dune nouvelle tâche",
+    description: "test dune nouvelle tâche",
+  },
+  degreeConfiguration: {
+    id: "1f0d6706-a60d-626c-ad95-fd4a3110da63",
+    degreeLevel: "MASTER",
+    degreeYear: "2A",
+    degreeField: "ADD",
+    skills: [
+      {
+        mainSkillCode: "MAINPO",
+        mainSkillId: "1f0d51fa-817a-6969-8d63-d98eece0b59b",
+        mainSkillName: "mainpo",
+        subSkills: [
+          {
+            id: "1f0d51fa-3026-6468-8d63-d98eece0b59b",
+            code: "SUBPO",
+            name: "subpo",
+          },
+        ],
+      },
+      {
+        mainSkillCode: "MAINSI",
+        mainSkillId: "1f0d5153-f7f6-6e63-8d63-d98eece0b59b",
+        mainSkillName: "mainsi",
+        subSkills: [
+          {
+            id: "1f0d5152-81f8-6b62-8d63-d98eece0b59b",
+            code: "SUBSI",
+            name: "subsi",
+          },
+        ],
+      },
+    ],
+  },
+  skills: [
+    {
+      mainSkill: {
+        name: "mainpo",
+        code: "MAINPO",
+        id: "1f0d51fa-817a-6969-8d63-d98eece0b59b",
+      },
+      subskills: [
+        {
+          name: "subpo",
+          code: "SUBPO",
+          id: "1f0d51fa-3026-6468-8d63-d98eece0b59b",
+        },
+      ],
+    },
+    {
+      mainSkill: {
+        name: "mainsi",
+        code: "MAINSI",
+        id: "1f0d5153-f7f6-6e63-8d63-d98eece0b59b",
+      },
+      subskills: [
+        {
+          name: "subsi",
+          code: "SUBSI",
+          id: "1f0d5152-81f8-6b62-8d63-d98eece0b59b",
+        },
+      ],
+    },
+  ],
+};
