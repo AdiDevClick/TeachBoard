0f88beaabd127c937a5bc920adac4dc1aa98b186
Adrien <adrienquijo@gmail.com>
2025-12-17 10:52:30 +0100

    Ajout de doc et le formId n'est plus optionnel

--- src/components/ClassCreation/diploma/degree-module/types/degree-module.types.ts
+++ src/components/ClassCreation/diploma/degree-module/types/degree-module.types.ts
@@ -2,9 +2,12 @@ import type DegreeModule from "@/components/ClassCreation/diploma/degree-module/
 import type { DegreeModuleFormSchema } from "@/models/degree-module.models";
 import type { FieldValues, useForm } from "react-hook-form";
 
+/**
+ * Props for DegreeModuleController component
+ */
 export type DegreeModuleProps<T extends FieldValues = DegreeModuleFormSchema> =
   {
-    formId?: string;
+    formId: string;
     form: ReturnType<typeof useForm<T>>;
     className?: string;
   } & Omit<Parameters<typeof DegreeModule>[0], "modalMode">;
