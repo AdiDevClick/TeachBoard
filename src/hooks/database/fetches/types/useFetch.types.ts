ba261cecf47e593f9342fc80b2e5f45eec9253ca
Adrien <adrienquijo@gmail.com>
2025-12-26 16:51:46 +0100

    maj du typage

--- src/hooks/database/fetches/types/useFetch.types.ts
+++ src/hooks/database/fetches/types/useFetch.types.ts
@@ -11,5 +11,10 @@ export type FetchParams = {
   sortBy: string;
   sortOrder: "asc" | "desc";
   cachedFetchKey?: [string, string];
-  dataReshapeFn?: (data: unknown, cachedDatas: unknown) => unknown;
+  dataReshapeFn?: (
+    data: unknown,
+    cachedDatas: unknown,
+    options?: unknown
+  ) => unknown;
+  reshapeOptions?: unknown;
 } & QueryKeyDescriptor<unknown, unknown>["1"];
