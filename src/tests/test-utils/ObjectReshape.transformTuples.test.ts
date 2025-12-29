ea09e8991acfe15ff8f50ccfe65a54cf39bd641d
Adrien <adrienquijo@gmail.com>
2025-12-29 09:58:34 +0100

    autre

--- src/tests/test-utils/ObjectReshape.transformTuples.test.ts
+++ src/tests/test-utils/ObjectReshape.transformTuples.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { ObjectReshape } from "../ObjectReshape";
+import { ObjectReshape } from "../../utils/ObjectReshape";
 
 describe("ObjectReshape - transformTuplesToGroups", () => {
   it("should transform dynamic object keys into array of groups", () => {
