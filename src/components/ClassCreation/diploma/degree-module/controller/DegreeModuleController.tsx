7607986e0305e8cc5f240cd96ebdfeb6c3fcf65d
Adrien <adrienquijo@gmail.com>
2026-01-01 23:03:05 +0100

    Le dynamicTagList pouvant maintenant recevoir des détails, le handleCommandSelection() permet maintenant de sauvegarder dans le formulaire un state plus détaillé - La liste non détaillée sera toujours celle envoyée au serveur - Code cleanup -

--- src/components/ClassCreation/diploma/degree-module/controller/DegreeModuleController.tsx
+++ src/components/ClassCreation/diploma/degree-module/controller/DegreeModuleController.tsx
@@ -1,4 +1,5 @@
 import type { DegreeModuleProps } from "@/components/ClassCreation/diploma/degree-module/types/degree-module.types";
+import type { CommandItemType } from "@/components/Command/types/command.types.ts";
 import { ControlledInputList } from "@/components/Inputs/LaballedInputForController.tsx";
 import { PopoverFieldWithCommands } from "@/components/Popovers/PopoverField.tsx";
 import { ControlledDynamicTagList } from "@/components/Tags/DynamicTag.tsx";
@@ -7,8 +8,10 @@ import { DEV_MODE, NO_CACHE_LOGS } from "@/configs/app.config.ts";
 import { degreeModuleCreationInputControllers } from "@/data/inputs-controllers.data.ts";
 import { useCommandHandler } from "@/hooks/database/classes/useCommandHandler";
 import type { MutationVariables } from "@/hooks/database/types/QueriesTypes.ts";
+import { UniqueSet } from "@/utils/UniqueSet.ts";
 import { useQueryClient } from "@tanstack/react-query";
 import { useCallback } from "react";
+import { useWatch } from "react-hook-form";
 
 /**
  * Controller component for creating a new degree skill.
@@ -42,6 +45,11 @@ export function DegreeModuleController({
   });
 
   const queryClient = useQueryClient();
+  const currentSkills =
+    useWatch({
+      control: form.control,
+      name: "skillListDetails",
+    }) || [];
 
   const resultsCallback = useCallback((keys: unknown[]) => {
     const cachedData = queryClient.getQueryData(keys ?? []);
@@ -79,19 +87,27 @@ export function DegreeModuleController({
     openingCallback(open, metaData, inputControllers);
   };
 
-  const handleCommandSelection = (value: string) => {
+  const handleCommandSelection = (
+    value: string,
+    commandItem: CommandItemType
+  ) => {
+    const currentSkills = new UniqueSet(
+      null,
+      form.getValues("skillListDetails") || []
+    );
     if (currentSkills.has(value)) {
       currentSkills.delete(value);
     } else {
-      currentSkills.add(value);
+      currentSkills.set(value, commandItem);
     }
-    form.setValue("skillList", Array.from(currentSkills), {
+    form.setValue("skillList", Array.from(currentSkills.keys()), {
+      shouldValidate: true,
+    });
+
+    form.setValue("skillListDetails", Array.from(currentSkills.entries()), {
       shouldValidate: true,
     });
   };
-
-  // Get the current skills from the form
-  const currentSkills = new Set(form.watch("skillList") || []);
 
   return (
     <form
@@ -110,7 +126,7 @@ export function DegreeModuleController({
         setRef={setRef}
         {...inputControllers[2]}
         observedRefs={observedRefs}
-        itemList={Array.from(currentSkills)}
+        itemList={currentSkills}
       />
       <PopoverFieldWithCommands
         multiSelection
@@ -128,93 +144,3 @@ export function DegreeModuleController({
     </form>
   );
 }
-{
-  /* <form id={id} className={className} onSubmit={form.handleSubmit(handleSubmit)}>
-  <ControlledInputList
-    items={inputControllers}
-    form={form}
-    setRef={setRef}
-    observedRefs={observedRefs}
-  />
-  {/* Display current skills */
-}
-// <Field>
-//   <ItemGroup id={`${pageId}-skills`} className="grid gap-2">
-//     <ItemTitle>Compétences sélectionnées</ItemTitle>
-//     {currentSkills.length > 0 ? (
-//       <Item variant={"default"} className="p-0">
-//         <ItemContent className="flex-row flex-wrap gap-2">
-//           <ListMapper items={currentSkills}>
-//             {(rawItem: string | { item: string; id?: string }) => {
-//               const item =
-//                 typeof rawItem === "string" ? rawItem : rawItem.item;
-
-//               const handleRemoveSkill = () => {
-//                 const updatedSkills = currentSkills.filter(
-//                   (skill: string) => skill !== item
-//                 );
-//                 form.setValue("skills", updatedSkills, {
-//                   shouldValidate: true,
-//                 });
-//               };
-
-//               return (
-//                 <ItemActions key={id} className="relative">
-//                   <Popover>
-//                     <PopoverTrigger asChild>
-//                       <Button id={id} size="sm" variant="outline">
-//                         {item}
-//                       </Button>
-//                     </PopoverTrigger>
-//                     <PopoverContent
-//                       side="top"
-//                       align="center"
-//                       sideOffset={8}
-//                       className="p-0.5 w-auto max-h-min-content"
-//                     >
-//                       <Button
-//                         size="sm"
-//                         variant="ghost"
-//                         onClick={handleRemoveSkill}
-//                         id={id + "-delete"}
-//                         aria-label={`Supprimer ${item}`}
-//                       >
-//                         Supprimer
-//                       </Button>
-//                       <PopoverArrow className="fill-popover" />
-//                     </PopoverContent>
-//                   </Popover>
-//                 </ItemActions>
-//               );
-//             }}
-//           </ListMapper>
-//         </ItemContent>
-//       </Item>
-//     ) : (
-//       <p className="text-sm text-muted-foreground">
-//         Aucune compétence sélectionnée
-//       </p>
-//     )}
-//   </ItemGroup>
-//   <FieldError errors={[form.formState.errors.skills]} />
-// </Field>
-// {/* Skill selection popover */}
-// <PopoverFieldWithCommands
-//   label={inputControllers[2].title}
-//   form={form}
-//   setRef={setRef}
-//   onSelect={(value: string) => {
-//     console.log("Selected Value in command : ", value);
-//     // Add the selected skill to the array if not already present
-//     if (value && !currentSkills.includes(value)) {
-//       const updatedSkills = [...currentSkills, value];
-//       form.setValue("skills", updatedSkills, { shouldValidate: true });
-//     }
-//   }}
-//   onOpenChange={handleOpening}
-//   observedRefs={observedRefs}
-//   onAddNewItem={handleAddNewItem}
-//   commandHeadings={resultsCallback([fetchParams.contentId, fetchParams.url])}
-//   {...inputControllers[2]}
-// />
-//</form>; */}
