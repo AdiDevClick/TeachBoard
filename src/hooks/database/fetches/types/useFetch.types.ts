bfa7f6099cc52c07232251c9f07910d80cc5ac69
Adrien <adrienquijo@gmail.com>
2026-01-03 20:18:59 +0100

    Refactor dataReshapeFn type definition for improved readability

--- src/hooks/database/fetches/types/useFetch.types.ts
+++ src/hooks/database/fetches/types/useFetch.types.ts
@@ -11,10 +11,6 @@ export type FetchParams = {
   sortBy: string;
   sortOrder: "asc" | "desc";
   cachedFetchKey?: [string, string];
-  dataReshapeFn?: (
-    data: unknown,
-    cachedDatas: unknown,
-    options?: unknown
-  ) => unknown;
+  dataReshapeFn?(data: unknown, cachedDatas: unknown, options?: unknown): unknown;
   reshapeOptions?: unknown;
 } & QueryKeyDescriptor<unknown, unknown>["1"];
