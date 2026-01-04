7dc96dd55ffa255b5e5b2e1f73f10a700f63a234
Adrien <adrienquijo@gmail.com>
2026-01-04 14:57:46 +0100

    Le readonly a été intégré au type directemnet

--- src/components/ClassCreation/diploma/degree-module-skill/types/degree-module-skill.types.ts
+++ src/components/ClassCreation/diploma/degree-module-skill/types/degree-module-skill.types.ts
@@ -4,7 +4,8 @@ import type { FieldValues, useForm } from "react-hook-form";
 
 export type DegreeModuleSkillProps<
   T extends FieldValues = DegreeModuleSkillFormSchema
-> = {
+> = Readonly<{
   formId?: string;
   form: ReturnType<typeof useForm<T>>;
-} & Omit<Parameters<typeof DegreeModuleSkill>[0], "modalMode">;
+}> &
+  Omit<Parameters<typeof DegreeModuleSkill>[0], "modalMode">;
