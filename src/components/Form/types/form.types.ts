f3c79335cb8108bdec846d9ce1d709234cbbcc91
Adrien <adrienquijo@gmail.com>
2026-01-04 21:47:37 +0100

    Fix de typing

--- src/components/Form/types/form.types.ts
+++ src/components/Form/types/form.types.ts
@@ -1,15 +1,13 @@
 import type { SimpleAvatarListProps } from "@/components/Avatar/types/avatar.types.ts";
-import { SimpleAddButtonWithToolTip } from "@/components/Buttons/SimpleAddButton.tsx";
 import type { SimpleAddButtonWithToolTipProps } from "@/components/Buttons/types/ButtonTypes.ts";
-import type { ComponentProps } from "react";
 
 /** Props for AvatarListWithLabelAndAddButton component */
-export type AvatarListWithLabelAndAddButtonProps = {
+export type AvatarListWithLabelAndAddButtonProps = Omit<
+  SimpleAvatarListProps & SimpleAddButtonWithToolTipProps,
+  "onClick"
+> & {
   label?: string;
-  onClick?: (
-    event: React.MouseEvent<HTMLButtonElement>,
-    rest: Omit<ComponentProps<typeof SimpleAddButtonWithToolTip>, "onClick">
-  ) => void;
   className?: string;
-} & SimpleAvatarListProps &
-  SimpleAddButtonWithToolTipProps;
+  /** Allow either the custom payload handler or a native mouse handler */
+  onClick?: SimpleAddButtonWithToolTipProps["onClick"];
+};
