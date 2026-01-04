f6f4b972c38856efadc0680a946adecccc374485
Adrien <adrienquijo@gmail.com>
2026-01-04 20:08:11 +0100

    Utilisation du typing directement depuis un extended

--- src/components/ClassCreation/teachers/types/search-teachers.types.ts
+++ src/components/ClassCreation/teachers/types/search-teachers.types.ts
@@ -1,7 +1,5 @@
-import type { UUID } from "@/api/types/openapi/common.types.ts";
 import type { SearchPrimaryTeacher } from "@/components/ClassCreation/teachers/SearchTeachers.tsx";
-import type { DetailedCommandItem } from "@/components/Command/types/command.types.ts";
-import type { ClassCreationFormSchema } from "@/models/class-creation.models.ts";
+import type { ClassCreationExtendedFormSchema } from "@/components/ClassCreation/types/class-creation.types.ts";
 import type { SearchPrimaryTeacherFormSchema } from "@/models/search-teachers.models.ts";
 import type { PageWithControllers } from "@/types/AppPagesInterface.ts";
 import type { UseFormReturn } from "react-hook-form";
@@ -17,19 +15,10 @@ export type SearchPrimaryTeacherControllerProps = Readonly<
 >;
 
 /**
- * Extended form schema to include detailed command items for primary teachers.
- *
- * @remarks This entry is NOT validated as it only serves as a mean of storing detailed command items associated with the primary teacher IDs within the main ClassCreationFormSchema.
- */
-export type SearchPrimaryTeacherExtendedForm = ClassCreationFormSchema & {
-  primaryTeacherValue?: Map<UUID, DetailedCommandItem>;
-};
-
-/**
  * Props for the SearchPrimaryTeacher component.
  */
 export type SearchPrimaryTeacherProps = Readonly<
   {
-    form?: UseFormReturn<SearchPrimaryTeacherExtendedForm>;
+    form?: UseFormReturn<ClassCreationExtendedFormSchema>;
   } & Omit<PageWithControllers, "inputControllers">
 >;
