132bef4f8875e96b3fa798a935905a0d9d9f7f2a
Adrien <adrienquijo@gmail.com>
2026-01-09 00:34:37 +0100

    fix: enhance DegreeItemController with toast notifications for loading and success states

--- src/components/ClassCreation/diploma/degree-item/controller/DegreeItemController.tsx
+++ src/components/ClassCreation/diploma/degree-item/controller/DegreeItemController.tsx
@@ -4,6 +4,10 @@ import { API_ENDPOINTS } from "@/configs/api.endpoints.config.ts";
 import { degreeCreationInputControllersField } from "@/data/inputs-controllers.data.ts";
 import { useCommandHandler } from "@/hooks/database/classes/useCommandHandler.ts";
 import type { MutationVariables } from "@/hooks/database/types/QueriesTypes.ts";
+import { toast } from "sonner";
+
+const loaderToasterId = "degree-item-creation-loader-toast";
+const creationToasterId = "degree-item-created-toast";
 
 /**
  * Controller component for creating a new degree item.
@@ -15,18 +19,40 @@ import type { MutationVariables } from "@/hooks/database/types/QueriesTypes.ts";
  * @param form - The react-hook-form instance.
  * @param className - Additional CSS classes for styling.
  * @param inputControllers - The input controllers for the form (this needs to be already validated by Zod Schema).
+ * @param submitDataReshapeFn - Function to reshape data before submission.
+ *
+ * @remarks The POST and GET ENDPOINTS share the same apiEndpoint and will be used from the input controllers directly
+ * This prevent the use of a useless and cognitive type check here.
  */
 export function DegreeItemController({
   pageId,
   inputControllers = degreeCreationInputControllersField,
-  className = "grid gap-4",
+  className,
   formId,
   form,
+  submitDataReshapeFn = API_ENDPOINTS.POST.CREATE_DEGREE.dataReshape,
 }: DegreeItemControllerProps) {
-  const { setRef, observedRefs, submitCallback } = useCommandHandler({
-    form,
-    pageId,
-  });
+  const { setRef, observedRefs, submitCallback, serverData, isLoading } =
+    useCommandHandler({
+      form,
+      pageId,
+      submitDataReshapeFn,
+    });
+
+  if (isLoading) {
+    toast.loading("Création en cours...", { id: loaderToasterId });
+  }
+
+  if (serverData?.degree) {
+    toast.dismiss(loaderToasterId);
+    const type = serverData.degree.type ?? "UNKNOWN";
+
+    const { typeMessage, defaultGenre } = getTypeMessage(type);
+
+    toast.success(`${typeMessage} ${defaultGenre} avec succès !`, {
+      id: creationToasterId,
+    });
+  }
 
   /**
    * Handle form submission
@@ -34,11 +60,11 @@ export function DegreeItemController({
    * @param variables - form variables
    */
   const handleSubmit = (variables: MutationVariables) => {
-    submitCallback(
-      variables,
-      null,
-      API_ENDPOINTS.POST.CREATE_DEGREE.dataReshape
-    );
+    // We use the same endPoints as GET from the inputs
+    // controllers. No need to specify POST endPoint again here.
+    submitCallback(variables, {
+      dataReshapeFn: submitDataReshapeFn,
+    });
   };
 
   const id = formId ?? pageId + "-form";
@@ -57,4 +83,35 @@ export function DegreeItemController({
       />
     </form>
   );
+}
+
+type DegreeTypeMessage = {
+  typeMessage: string;
+  defaultGenre: string;
+};
+
+/**
+ * Get the message type based on degree type
+ *
+ * @param type - degree type
+ */
+function getTypeMessage(type: string): DegreeTypeMessage {
+  const defined = { typeMessage: "", defaultGenre: "créé" };
+
+  switch (type) {
+    case "FIELD":
+      defined.typeMessage = "Nom du métier / domaine";
+      break;
+    case "DEGREE":
+      defined.typeMessage = "Diplôme";
+      break;
+    case "YEAR":
+      defined.typeMessage = "Année d'étude";
+      defined.defaultGenre = "créée";
+      break;
+    default:
+      defined.typeMessage = "Élément";
+  }
+
+  return defined;
 }
