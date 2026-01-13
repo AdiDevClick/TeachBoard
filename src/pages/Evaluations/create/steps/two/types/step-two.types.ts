fb0fb58168eb0e2e753924db18f390877d693594
Adrien <adrienquijo@gmail.com>
2026-01-13 20:30:26 +0100

    feat: extend StepTwoControllerProps to include user, students, and selectedClass

--- src/pages/Evaluations/create/steps/two/types/step-two.types.ts
+++ src/pages/Evaluations/create/steps/two/types/step-two.types.ts
@@ -1,3 +1,5 @@
+import type { StepsCreationState } from "@/hooks/store/StepsCreationStore.ts";
+import type { User } from "@/hooks/store/types/store.types.ts";
 import type { AttendanceRecordCreationFormSchema } from "@/models/attendance-record-creation.models.ts";
 import type { StepTwo } from "@/pages/Evaluations/create/steps/two/StepTwo.tsx";
 import type { AppControllerInterface } from "@/types/AppControllerInterface.ts";
@@ -7,4 +9,8 @@ import type { AppControllerInterface } from "@/types/AppControllerInterface.ts";
  */
 export type StepTwoControllerProps =
   AppControllerInterface<AttendanceRecordCreationFormSchema> &
-    Omit<Parameters<typeof StepTwo>[0], "modalMode">;
+    Omit<Parameters<typeof StepTwo>[0], "modalMode"> & {
+      user?: User;
+      students?: StepsCreationState["students"];
+      selectedClass?: StepsCreationState["selectedClass"];
+    };
