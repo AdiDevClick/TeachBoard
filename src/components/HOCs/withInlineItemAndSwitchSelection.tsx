547e85579ebaccb82c3a24f88ece227655800e77
Adrien <adrienquijo@gmail.com>
2026-01-14 14:09:39 +0100

    Feature : Le composant contient maintenant un setter interne sur le clic du switch - Permettant de s'assurer que le dropdown est désactivé si le switch n'est pas actif - Il est aussi possible d'utiliser une props handleSwitchClick() externe au composant, pour avoir accès à quelques informations sur l'item sélectionné - Le payload passé à ce handler permet aussi de modifier le state interne au composant via setIsSelected(boolean). Cela va permettre de forcer un état inverse si désiré -

--- src/components/HOCs/withInlineItemAndSwitchSelection.tsx
+++ src/components/HOCs/withInlineItemAndSwitchSelection.tsx
@@ -1,4 +1,7 @@
-import type { InlineItemAndSwitchSelectionProps } from "@/components/HOCs/types/with-inline-item-and-switch.types.ts";
+import type {
+  InlineItemAndSwitchSelectionPayload,
+  InlineItemAndSwitchSelectionProps,
+} from "@/components/HOCs/types/with-inline-item-and-switch.types.ts";
 import {
   Item,
   ItemActions,
@@ -10,7 +13,8 @@ import {
   debugLogs,
   inlineItemAndSwitchSelectionPropsInvalid,
 } from "@/configs/app-components.config.ts";
-import type { ComponentProps, ComponentType } from "react";
+import { preventDefaultAndStopPropagation } from "@/utils/utils.ts";
+import { useState, type ComponentType, type MouseEvent } from "react";
 
 /**
  * Wrap a component to be displayed inline within an Item with a Switch.
@@ -30,23 +34,58 @@ export function withInlineItemAndSwitchSelection<T extends object>(
   Wrapped: ComponentType<T>
 ) {
   return function Component(props: T & InlineItemAndSwitchSelectionProps) {
+    const [isSelected, setIsSelected] = useState(props.isSelected ?? false);
     if (inlineItemAndSwitchSelectionPropsInvalid(props)) {
       debugLogs("withInlineItemAndSwitchSelection");
       return null;
     }
 
-    const { title, onSwitchClick, ...restProps } = props;
+    const { onSwitchClick } = props;
+
+    /**
+     * Handle the switch click event.
+     *
+     * @remarks The event is prevented from propagating by default.
+     *
+     * @description If you pass an onSwitchClick prop, the internal state will already be changed before this handler is called.
+     * The click is enriched with a payload containing the current local setter and item details.
+     *
+     * @param e - The mouse event triggered by clicking the switch.
+     *
+     * @example
+     * ```tsx
+     * const handleSwitchClick = (e, payload) => {
+     *   console.log("Switch clicked for item:", payload.title, "New state:", payload.isSelected);
+     *   // You can also use payload.setIsSelected to change the state from here if needed
+     *  payload.setIsSelected(!payload.isSelected); // Force the reverse of the current state
+     * }
+     * ```
+     */
+    const handleSwitchClick = (e: MouseEvent<HTMLButtonElement>) => {
+      preventDefaultAndStopPropagation(e);
+      setIsSelected(!isSelected);
+
+      const payload: InlineItemAndSwitchSelectionPayload = {
+        id: props.id ?? "unknown-id",
+        title: props.title,
+        isSelected: !isSelected,
+        index: props.index ?? "unknown-index",
+        setIsSelected,
+      };
+
+      onSwitchClick?.(e, payload);
+    };
 
     return (
       <Item>
         <ItemContent>
-          <ItemTitle>{title}</ItemTitle>
+          <ItemTitle>{props.title}</ItemTitle>
         </ItemContent>
         <ItemActions>
-          <Switch onClick={onSwitchClick} />
+          <Switch onClick={handleSwitchClick} checked={isSelected} />
         </ItemActions>
         <ItemContent>
-          <Wrapped {...(restProps as T)} />
+          <Wrapped {...props} disabled={!isSelected} />
         </ItemContent>
       </Item>
     );
