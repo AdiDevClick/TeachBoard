d37aafa71a9430223790ec5a8df1f576fbe022a9
Adrien <adrienquijo@gmail.com>
2026-01-14 14:09:46 +0100

    feat: update InlineItemAndSwitchSelectionProps to include detailed payload structure

--- src/components/HOCs/types/with-inline-item-and-switch.types.ts
+++ src/components/HOCs/types/with-inline-item-and-switch.types.ts
@@ -1,7 +1,20 @@
+import type { MouseEvent, SetStateAction } from "react";
+
+export type InlineItemAndSwitchSelectionPayload = {
+  id?: string;
+  title: string;
+  isSelected?: boolean;
+  index?: string | number;
+  setIsSelected: (selected: SetStateAction<boolean>) => void;
+};
+
 /**
  * Expected props for withInlineItemAndSwitchSelection HOC.
  */
-export type InlineItemAndSwitchSelectionProps = {
-  title: string;
-  onSwitchClick?: () => void;
-};
+export type InlineItemAndSwitchSelectionProps =
+  InlineItemAndSwitchSelectionPayload & {
+    onSwitchClick?: (
+      e: MouseEvent<HTMLButtonElement>,
+      payload: InlineItemAndSwitchSelectionPayload
+    ) => void;
+  };
