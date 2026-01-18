f5e6d6f223a906a734ff057dbf3dc50defbf90bc
Adrien <adrienquijo@gmail.com>
2026-01-18 17:52:48 +0100

    Le clic d'un module peut maintenant remonter au controlleur et donner toutes les informations nécessaire à la sélection

--- src/pages/Evaluations/create/steps/three/controller/StepThreeController.tsx
+++ src/pages/Evaluations/create/steps/three/controller/StepThreeController.tsx
@@ -1,6 +1,8 @@
 import { EvaluationRadioItemList } from "@/components/Radio/EvaluationRadioItem.tsx";
+import type { EvaluationRadioItemProps } from "@/components/Radio/types/radio.types.ts";
 import { RadioGroup } from "@/components/ui/radio-group.tsx";
 import type { StepThreeControllerProps } from "@/pages/Evaluations/create/steps/three/types/step-three.types.ts";
+import type { MouseEvent } from "react";
 
 export function StepThreeController({
   pageId,
@@ -15,11 +17,19 @@ export function StepThreeController({
   formId,
   modules,
 }: StepThreeControllerProps) {
-  console.log(tasks);
+  const handleOnClick = (
+    e: MouseEvent<HTMLDivElement>,
+    props: EvaluationRadioItemProps,
+  ) => {
+    const selectedModule = modules[props.index];
+    console.log("Clicked item:", props);
+    console.log("event :", e);
+    console.log("module : ", selectedModule);
+  };
   return (
     <form id={formId}>
       <RadioGroup>
-        <EvaluationRadioItemList items={modules} />
+        <EvaluationRadioItemList items={modules} itemClick={handleOnClick} />
       </RadioGroup>
     </form>
   );
