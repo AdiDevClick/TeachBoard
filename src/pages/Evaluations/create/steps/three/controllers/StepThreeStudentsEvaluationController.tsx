afd206d4a8fa31ad10d1e5bb99bb18a05d5c3411
Adrien <adrienquijo@gmail.com>
2026-01-25 16:14:12 +0100

    autre

--- src/pages/Evaluations/create/steps/three/controllers/StepThreeStudentsEvaluationController.tsx
+++ src/pages/Evaluations/create/steps/three/controllers/StepThreeStudentsEvaluationController.tsx
@@ -1,4 +1,3 @@
-import { useEvaluationStepsCreationStore } from "@/api/store/EvaluationStepsCreationStore.ts";
 import "@/assets/css/Slider.scss";
 import { ListMapper } from "@/components/Lists/ListMapper.tsx";
 import type { EvaluationRadioItemProps } from "@/components/Radio/types/radio.types.ts";
