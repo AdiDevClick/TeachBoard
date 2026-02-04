9de25a27f3c67cfd3eb3aa0ebe0d9cae97e0d4ba
Adrien <adrienquijo@gmail.com>
2026-02-04 12:50:08 +0100

    Refactor Step Four Controller to integrate new components - Il peut maintenant afficher la moyenne des élèves et une input permet de modifier ce chiffre pour coller à la demande PO

--- src/features/evaluations/create/steps/four/controller/StepFourController.tsx
+++ src/features/evaluations/create/steps/four/controller/StepFourController.tsx
@@ -1,4 +1,4 @@
-import { summaryPageContent } from "@/assets/css/SummaryPage.module.scss";
+import { ControlledLabelledInput } from "@/components/Inputs/LaballedInputForController";
 import { ListMapper } from "@/components/Lists/ListMapper";
 import {
   EvaluationSlider,
@@ -11,10 +11,17 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { Badge } from "@/components/ui/badge";
 import { CardDescription } from "@/components/ui/card";
-import { ItemTitle } from "@/components/ui/item";
+import {
+  Item,
+  ItemContent,
+  ItemDescription,
+  ItemTitle,
+} from "@/components/ui/item";
+import { Textarea } from "@/components/ui/textarea";
 import { useEvaluationStepsCreationStore } from "@/features/evaluations/create/store/EvaluationStepsCreationStore";
-import type { ComponentProps } from "react";
+import { useMemo, type ComponentProps } from "react";
 import { useShallow } from "zustand/shallow";
 
 type StepFourControllerProps = {
@@ -39,29 +46,41 @@ export function StepFourController({ pageId, form }: StepFourControllerProps) {
     useShallow((state) => state.getAllNonPresentStudents()),
   );
 
-  let studentsPresence = Array.from(nonPresentStudents.entries());
+  const allStudentsAverageScores = useEvaluationStepsCreationStore(
+    useShallow((state) => state.getAllStudentsAverageScores()),
+  );
 
-  if (studentsPresence.length === 0) {
-    studentsPresence = [["Aucun", {}]];
-  }
+  /**
+   * Generates a list of absent students names to display
+   *
+   * @description Saves ids instead of names in the form for validation.
+   */
+  const presenceMemo = useMemo(() => {
+    let studentsPresence = Array.from(nonPresentStudents.values());
+    let studentsPresenceIds = Array.from(nonPresentStudents.keys());
+    if (studentsPresence.length === 0) {
+      studentsPresence = [["Aucun", {}]];
+      studentsPresenceIds = ["none"];
+    }
+
+    form.setValue("absence", studentsPresenceIds);
+
+    return studentsPresence;
+  }, [nonPresentStudents]);
 
   return (
     <>
       <CardDescription>{"Les modules"}</CardDescription>
-      <Accordion
-        type="single"
-        collapsible
-        // defaultValue="shipping"
-        className={summaryPageContent}
-      >
+      <Accordion type="single" collapsible className={"summaryPageContent"}>
         <ListMapper items={modules}>
           {(module) => {
             return (
-              <AccordionItem value={module.id}>
+              <AccordionItem key={module.id} value={module.id}>
                 <AccordionTrigger>{module.name}</AccordionTrigger>
                 <AccordionContent>
                   <ListMapper items={Array.from(module.subSkills.values())}>
                     {(subSkill) => {
+                      console.log("jai trigger un render de subskills");
                       if (subSkill.isCompleted && !subSkill.isDisabled) {
                         return (
                           <div
@@ -75,13 +94,12 @@ export function StepFourController({ pageId, form }: StepFourControllerProps) {
                                 subSkill.id,
                               )}
                               optional={(student) => {
-                                return {
-                                  value: getStudentScoreForSubSkill(
-                                    student.id,
-                                    subSkill?.id,
-                                    module?.id,
-                                  ),
-                                };
+                                const value = getStudentScoreForSubSkill(
+                                  student.id,
+                                  subSkill.id,
+                                  module.id,
+                                );
+                                return { value };
                               }}
                               inert
                               // onValueChange={handleValueChange}
@@ -208,18 +226,58 @@ export function StepFourController({ pageId, form }: StepFourControllerProps) {
         </AccordionItem>
       </Accordion>
       {/* <CardDescription>{"Elèves absents"}</CardDescription> */}
-      <ControlledDynamicTagList
-        form={form}
-        // {...sharedCallbacksMemo.commonObsProps}
-        // {...controllers.dynamicListControllers}
-        name="step-four-controller"
-        pageId={pageId}
-        title={"Elèves absents aujourd'hui"}
-        itemList={studentsPresence}
-        inert
-        displayCRUD={false}
-        // onRemove={handleDeletingTask}
-      />
+      <Item>
+        <ItemContent>
+          <ItemTitle>{"Note globale des élèves"}</ItemTitle>
+          <ItemDescription>
+            {"Moyenne générale des notes pour chaque élève évalué(e)."}
+          </ItemDescription>
+          {Array.from(allStudentsAverageScores.entries()).map(
+            ([studentId, studentDetails]) => {
+              console.log(studentId, studentDetails);
+              return (
+                <div
+                  key={studentId}
+                  className="grid grid-cols-9 gap-2 justify-items-center items-center "
+                >
+                  <Badge className="m-4 justify-self-start col-start-1 col-end-6">
+                    {studentDetails.name}
+                  </Badge>
+                  <p className="col-start-6 col-end-8">{"Moyenne : "}</p>
+                  <ControlledLabelledInput
+                    className="col-start-8 col-end-9"
+                    name={studentId}
+                    form={form}
+                    defaultValue={studentDetails.score / 5}
+                    // value={averageScore[0] / 5}
+                  />
+                  <p className="col-start-9">{"/20"}</p>
+                </div>
+              );
+            },
+          )}
+        </ItemContent>
+      </Item>
+      <Item>
+        <ControlledDynamicTagList
+          form={form}
+          // {...sharedCallbacksMemo.commonObsProps}
+          // {...controllers.dynamicListControllers}
+          name="step-four-controller"
+          pageId={pageId}
+          title={"Elèves absents aujourd'hui"}
+          itemList={presenceMemo}
+          inert
+          displayCRUD={false}
+          // onRemove={handleDeletingTask}
+        />
+      </Item>
+      <Item>
+        <ItemContent>
+          <ItemTitle>{"Observations"}</ItemTitle>
+          <Textarea />
+        </ItemContent>
+      </Item>
     </>
   );
 }
