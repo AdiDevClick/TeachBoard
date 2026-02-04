164b6b934c201d44fcd5c8266fbc113857dc4a34
Adrien <adrienquijo@gmail.com>
2026-02-04 19:12:30 +0100

    Add task template title and footer configurations

---
+++ src/features/class-creation/components/TaskTemplateCreation/config/task-template-creation.configs.ts
@@ -0,0 +1,9 @@
+export const TASK_TEMPLATE_TITLE = {
+  title: "Création de nouvelles tâches",
+  description: "Ces tâches pourront être associées aux modules de compétences.",
+};
+
+export const TASK_TEMPLATE_FOOTER = {
+  submitText: "Ajouter",
+  cancelText: "Annuler",
+};
