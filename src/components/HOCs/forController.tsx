16a1c7a0827812d86e44c72f15955bd1cd1092d6
Adrien <adrienquijo@gmail.com>
2026-02-07 18:48:53 +0100

    ForController est maintenant un HOC centralisé car réutilisable

---
+++ src/components/HOCs/forController.tsx
@@ -0,0 +1,44 @@
+import {
+  debugLogs,
+  forControllerContainsInvalid,
+} from "@/configs/app-components.config";
+import type { FieldTypes } from "@/types/MainTypes";
+import type { ComponentType } from "react";
+import type { FieldValues } from "react-hook-form";
+
+/**
+ * Higher-order component to pre-configure some components to be ready for use with react-hook-form Controller.
+ *
+ * @param WrapperComponent - The component to wrap with Controller integration.
+ * @param field - The field props from react-hook-form Controller.
+ * @param fieldState - The field state from react-hook-form Controller.
+ * @param props - Other props to pass to the wrapped component.
+ *
+ * @returns A new component that is pre-configured for use with react-hook-form Controller.
+ */
+export function forController<P>(WrapperComponent: ComponentType<P>) {
+  return function Component(props: P & FieldTypes<FieldValues>) {
+    if (forControllerContainsInvalid(props)) {
+      debugLogs("[forController]");
+      return null;
+    }
+
+    const { field, fieldState, ...rest } = props;
+
+    // const handleValueChange = (value: UUID, meta?: VerticalSelectMetaData) => {
+    //       field.onChange(value);
+    //       onValueChange?.(value, meta);
+    //     };
+
+    return (
+      <WrapperComponent
+        {...(rest as P)}
+        {...field}
+        // value={field.value ?? ""}
+        // onValueChange={handleValueChange}
+        // name={field.name} // NOTE: name is already included in {...field}, but can be explicitly passed if needed by the popover component
+        aria-invalid={fieldState.invalid}
+      />
+    );
+  };
+}
