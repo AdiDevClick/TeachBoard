f9da1481ef50976fad4783152a0cee13d063b1c2
Adrien <adrienquijo@gmail.com>
2026-02-07 18:51:35 +0100

    Fix import path for ControlledInputList in DegreeModuleSkillController component

--- src/features/class-creation/components/DegreeModuleSkill/controllers/DegreeModuleSkillController.tsx
+++ src/features/class-creation/components/DegreeModuleSkill/controllers/DegreeModuleSkillController.tsx
@@ -1,4 +1,4 @@
-import { ControlledInputList } from "@/components/Inputs/LaballedInputForController.tsx";
+import { ControlledInputList } from "@/components/Inputs/exports/labelled-input";
 import { API_ENDPOINTS } from "@/configs/api.endpoints.config.ts";
 import { HTTP_METHODS } from "@/configs/app.config.ts";
 import { degreeSubSkillsCreationInputControllers } from "@/features/class-creation/components/DegreeModuleSkill/forms/degree-module-skill-inputs";
