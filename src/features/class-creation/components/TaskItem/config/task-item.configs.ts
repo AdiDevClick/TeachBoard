c3c0be2c2965fa12656ebe8815315eb814c27def
Adrien <adrienquijo@gmail.com>
2026-02-08 01:18:00 +0100

    Refactor TaskItem component: extract title and footer configurations into a separate config file for improved maintainability and readability

---
+++ src/features/class-creation/components/TaskItem/config/task-item.configs.ts
@@ -0,0 +1,10 @@
+/**
+ * @fileoverview This file contains the TaskItem component and its related configurations for the class creation feature.
+ */
+
+export const TASK_ITEM_TITLE = {
+  title: "Création d'une nouvelle tâche",
+  description: "Ajoutez une nouvelle tâche pour les classes.",
+};
+
+export const TASK_ITEM_FOOTER = { submitText: "Créer", cancelText: "Annuler" };
--- src/features/class-creation/components/TaskTemplateCreation/config/task-template-creation.configs.ts
+++ src/features/class-creation/components/TaskTemplateCreation/config/task-template-creation.configs.ts
@@ -1,3 +1,7 @@
+/**
+ * @fileoverview This file contains the TaskTemplateCreation component and its related configurations for the class creation feature.
+ */
+
 export const TASK_TEMPLATE_TITLE = {
   title: "Création de nouvelles tâches",
   description: "Ces tâches pourront être associées aux modules de compétences.",
