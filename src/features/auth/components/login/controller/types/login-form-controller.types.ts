e85c194ccf731e3a3906ac8664d72d915a60f05a
Adrien <adrienquijo@gmail.com>
2026-02-08 22:22:28 +0100

    feat: add LoginFormController component and associated types for login functionality

---
+++ src/features/auth/components/login/controller/types/login-form-controller.types.ts
@@ -0,0 +1,24 @@
+import type { API_ENDPOINTS } from "@/configs/api.endpoints.config";
+import type LoginView from "@/features/auth/components/login/LoginView";
+import type { LoginFormSchema } from "@/features/auth/components/login/models/login.models";
+import type { ForgottenPw } from "@/features/auth/types/auth-types";
+import type { AppControllerInterface } from "@/types/AppControllerInterface";
+import type { FieldValues } from "react-hook-form";
+/**
+ * @filedescription Types for the LoginFormController component, which handles the logic for the login form in the authentication flow.
+ */
+
+/**
+ * Props for the LoginFormController component.
+ */
+export type LoginFormControllerProps<
+  TForm extends FieldValues = LoginFormSchema,
+> = Readonly<
+  AppControllerInterface<
+    TForm,
+    typeof API_ENDPOINTS.POST.AUTH.LOGIN.endpoint,
+    typeof API_ENDPOINTS.POST.AUTH.LOGIN.dataReshape
+  > &
+    Omit<Parameters<typeof LoginView>[0], "modalMode"> &
+    ForgottenPw
+>;
--- src/features/auth/components/login/types/login-forms.types.ts
+++ src/features/auth/components/login/types/login-forms.types.ts
@@ -1,11 +1,9 @@
-import type { API_ENDPOINTS } from "@/configs/api.endpoints.config";
-import type LoginView from "@/features/auth/components/login/LoginView";
 import type {
   LoginFormSchema,
   RecoveryFormSchema,
 } from "@/features/auth/components/login/models/login.models";
-import type { AppControllerInterface } from "@/types/AppControllerInterface.ts";
-import type { Dispatch, MouseEvent, SetStateAction } from "react";
+import type { ForgottenPwLinkParams } from "@/features/auth/types/auth-types";
+import type { MouseEvent } from "react";
 import type { UseFormReturn } from "react-hook-form";
 
 /**
@@ -13,36 +11,5 @@ import type { UseFormReturn } from "react-hook-form";
  */
 export type HandleRecoverPasswordClickParams = {
   e: MouseEvent<HTMLAnchorElement>;
-  isPwForgotten: boolean;
-  setIsPwForgotten: Dispatch<SetStateAction<boolean>>;
   form: UseFormReturn<LoginFormSchema> | UseFormReturn<RecoveryFormSchema>;
-};
-
-type ForgottenPwLinkParams = {
-  isPwForgotten: boolean;
-  setIsPwForgotten: Dispatch<SetStateAction<boolean>>;
-};
-
-type ForgottenPwAndDefaultLinkTexts = {
-  textToDisplay: {
-    defaultText: string;
-    pwForgottenLinkTo: string;
-    buttonText: string;
-  };
-};
-
-type ForgottenPw = ForgottenPwLinkParams & ForgottenPwAndDefaultLinkTexts;
-
-/**
- * Props for the LoginFormController component.
- */
-
-export type LoginFormControllerProps = Readonly<
-  AppControllerInterface<
-    LoginFormSchema,
-    typeof API_ENDPOINTS.POST.AUTH.LOGIN.endpoint,
-    typeof API_ENDPOINTS.POST.AUTH.LOGIN.dataReshape
-  > &
-    Omit<Parameters<typeof LoginView>[0], "modalMode"> &
-    ForgottenPw
->;
+} & ForgottenPwLinkParams;
