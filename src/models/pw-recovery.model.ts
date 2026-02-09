7341034fe740d23926c9e423a630e0dd84f942b3
Adrien <adrienquijo@gmail.com>
2026-02-09 17:08:59 +0100

    feat: refactor login and password recovery schemas to use fieldData for error messages

--- src/models/pw-recovery.model.ts
+++ src/models/pw-recovery.model.ts
@@ -1,11 +1,28 @@
+import type { InputItem } from "@/types/AppInputControllerInterface";
 import z from "zod";
 
+const fieldData = {
+  identifierMinErrorMessage:
+    "Votre identifiant doit contenir au moins 5 caractères.",
+  identifierMaxErrorMessage:
+    "Votre identifiant ne peut contenir plus de 64 caractères.",
+  identifierRequiredErrorMessage: "L'adresse email est requise.",
+  invalidEmailErrorMessage: "Veuillez entrer une adresse email valide.",
+};
+
 /** Validation schema for the password recovery form */
-export const pwRecoverySchema = z.object({
-  identifier: z
-    .email("Veuillez entrer une adresse email valide.")
-    .min(5, "L'adresse email doit contenir au moins 5 caractères.")
-    .max(254, "L'adresse email ne peut contenir plus de 254 caractères.")
-    .nonempty("L'adresse email est requise.")
-    .toLowerCase(),
-});
+export const pwRecoverySchema = (data: typeof fieldData) =>
+  z.object({
+    identifier: z
+      .email(data.invalidEmailErrorMessage)
+      .min(5, data.identifierMinErrorMessage)
+      .max(254, data.identifierMaxErrorMessage)
+      .nonempty(data.identifierRequiredErrorMessage)
+      .toLowerCase(),
+  });
+
+export type PwRecoveryFormSchema = z.infer<typeof pwRecoveryFormSchema>;
+
+export const pwRecoveryFormSchema = pwRecoverySchema(fieldData);
+
+export type PwRecoveryInputItem = InputItem<PwRecoveryFormSchema>;
