f6643c7ee8169c0c3cc52609d54befc402caf59b
Adrien <adrienquijo@gmail.com>
2026-02-09 17:05:31 +0100

    feat: update FooterFields component to include pageId prop and refactor onClick handling - Le footer peut correctement appeler le signup

--- src/features/auth/components/main/components/Footers/FooterFields.tsx
+++ src/features/auth/components/main/components/Footers/FooterFields.tsx
@@ -2,6 +2,7 @@ import { AppFieldDescriptionWithLink } from "@/components/Fields/AppFieldDescrip
 import { Button } from "@/components/ui/button";
 import { Field } from "@/components/ui/field";
 import type { FooterFieldsProps } from "@/features/auth/components/main/components/Footers/types/footer-fields.types";
+import { useAppForm } from "@/features/auth/hooks/useAppForm";
 import { useFormState } from "react-hook-form";
 
 /**
@@ -10,15 +11,17 @@ import { useFormState } from "react-hook-form";
  * @param form - The form object returned by the useForm hook, containing methods and state for managing the form.
  * @param formId - The ID of the form, used to associate the submit button with the form.
  * @param textToDisplay - An object containing text for the button and link, allowing for dynamic display based on the current mode (login or password recovery).
+ * @param pageId - The identifier of the page/context where the form is used; forwarded to useAppForm for analytics/command handling.
  * @param onClick - Optional click handler for the link, allowing for custom behavior when toggling between modes.
  */
-export function FooterFields<TFormValues extends Record<string, unknown>>({
+export function FooterFields({
   form,
   formId,
   textToDisplay,
-  onClick,
-}: FooterFieldsProps<TFormValues>) {
-  const { isValid } = useFormState<TFormValues>({ control: form.control });
+  pageId,
+}: Readonly<FooterFieldsProps>) {
+  const { isValid } = useFormState({ control: form.control });
+  const { newItemCallback } = useAppForm({ form, pageId });
   return (
     <Field>
       <Button type="submit" disabled={!isValid} form={formId}>
@@ -27,7 +30,12 @@ export function FooterFields<TFormValues extends Record<string, unknown>>({
       <AppFieldDescriptionWithLink
         linkText="Inscrivez-vous ici"
         linkTo="/signup"
-        onClick={onClick}
+        onClick={(e) =>
+          newItemCallback({
+            e,
+            task: "signup",
+          })
+        }
       >
         {"Vous n'avez pas de compte ? "}
       </AppFieldDescriptionWithLink>
