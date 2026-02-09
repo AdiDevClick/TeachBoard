21033bc07c6e68631f8347d3030eb809bc67d40b
Adrien <adrienquijo@gmail.com>
2026-02-09 17:11:02 +0100

    fix: add modalMode property to contentProps in LoginView modal - Cela fix un bug de sémantique qui attend des dialog et non pas des card

--- src/pages/AllModals/AppModals.tsx
+++ src/pages/AllModals/AppModals.tsx
@@ -43,6 +43,7 @@ const modals = defineStrictModalsList([
     modalContent: LoginView,
     contentProps: {
       inputControllers: inputLoginControllers,
+      modalMode: true,
     },
   },
   {
