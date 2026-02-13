651cecb26ee09cb99e129d62d33febea641f0e59
Adrien <adrienquijo@gmail.com>
2026-02-13 14:41:47 +0100

    feat: enhance lastUserActivity management with UniqueSet and detailed tracking - Cela va permettre de mieux rediriger les utilisateurs

--- src/api/store/types/app-store.types.ts
+++ src/api/store/types/app-store.types.ts
@@ -4,6 +4,8 @@ import type {
   UUID,
 } from "@/api/types/openapi/common.types.ts";
 import type { USER_ACTIVITIES } from "@/configs/app.config.ts";
+import type { UniqueSet } from "@/utils/UniqueSet";
+import type { FormMethod } from "react-router-dom";
 export type AppRoles = "ADMIN" | "TEACHER" | "STUDENT" | "STAFF";
 
 /**
@@ -35,13 +37,24 @@ export interface User {
 export type LastUserActivityType =
   (typeof USER_ACTIVITIES)[keyof typeof USER_ACTIVITIES];
 
+export type LastUserActivity = UniqueSet<
+  LastUserActivityType,
+  LastUserActivityDetails
+>;
+export type LastUserActivityDetails = {
+  url: string;
+  previousUrl?: string;
+  endpoint?: string;
+  method?: FormMethod;
+  type?: string;
+};
 /**
  * Persisting Application store.
  *
  * @remark Setters are not needed in the type definition and are managed by Zustand/combine internally
  */
 export interface AppStore {
-  lastUserActivity: LastUserActivityType;
+  lastUserActivity: LastUserActivity;
   isLoggedIn: boolean;
   user: User | null;
   sessionSynced: boolean;
