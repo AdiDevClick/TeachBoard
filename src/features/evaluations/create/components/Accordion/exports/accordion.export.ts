00a68a84cf88dcccdc62fe8d88f067ac6b97627a
Adrien <adrienquijo@gmail.com>
2026-02-17 19:53:46 +0100

    Add createComponentName calls for SubSkillWithStudentsList and AccordionItemWithSubSkillWithStudents

--- src/features/evaluations/create/components/Accordion/exports/accordion.export.ts
+++ src/features/evaluations/create/components/Accordion/exports/accordion.export.ts
@@ -1,6 +1,7 @@
 import withListMapper from "@/components/HOCs/withListMapper";
 import { SubSkillWithStudents } from "@/features/evaluations/create/components/Accordion/SubSkillWithStudents";
 import { withAccordionItem } from "@/features/evaluations/create/components/HOCs/withAccordionItem";
+import { createComponentName } from "@/utils/utils";
 /**
  * @fileoverview Exports the SubSkillWithStudentsList component, which is a list of SubSkillWithStudents components wrapped with the withListMapper HOC.
  */
@@ -11,6 +12,11 @@ import { withAccordionItem } from "@/features/evaluations/create/components/HOCs
  * @exports SubSkillWithStudentsList - A list of SubSkillWithStudents components.
  */
 export const SubSkillWithStudentsList = withListMapper(SubSkillWithStudents);
+createComponentName(
+  "withListMapper",
+  "SubSkillWithStudentsList",
+  SubSkillWithStudentsList,
+);
 
 /**
  * An accordion item that contains a subskill whose displays a list of students and their slider score.
@@ -18,10 +24,20 @@ export const SubSkillWithStudentsList = withListMapper(SubSkillWithStudents);
 export const AccordionItemWithSubSkillWithStudents = withAccordionItem(
   SubSkillWithStudentsList,
 );
+createComponentName(
+  "withAccordionItem",
+  "AccordionItemWithSubSkillWithStudents",
+  AccordionItemWithSubSkillWithStudents,
+);
 
 /**
  * A list of acordion items that for each item, contains a subskill whose displays a list of students and their slider score.
  */
 export const AccordionItemWithSubSkillWithStudentsList = withListMapper(
   AccordionItemWithSubSkillWithStudents,
+);
+createComponentName(
+  "withListMapper",
+  "AccordionItemWithSubSkillWithStudentsList",
+  AccordionItemWithSubSkillWithStudentsList,
 );
