c03fa5b5aaf99c851d79d92784ad56a520dec6d9
Adrien <adrienquijo@gmail.com>
2026-02-22 19:20:55 +0100

    Le leftcontent étant géré dans <LeftSide/>, le contenu n'est plus nécessaire - Code cleanup -

--- src/features/evaluations/create/steps/three/components/step-three-functionnal-wrappers.functions.tsx
+++ src/features/evaluations/create/steps/three/components/step-three-functionnal-wrappers.functions.tsx
@@ -1,26 +1,5 @@
 import type { DescriptionChangeProps } from "@/features/evaluations/create/steps/three/components/types/wrappers-functions.types";
 import { STEP_THREE_SUBSKILLS_SELECTION_TITLE_PROPS } from "@/features/evaluations/create/steps/three/config/step-three.configs";
-import { StepThreeSubskillsSelectionController } from "@/features/evaluations/create/steps/three/controllers/StepThreeSubskillsSelectionController";
-import type { StepThreeSubskillsSelectionControllerProps } from "@/features/evaluations/create/steps/three/types/step-three.types";
-
-/**
- * Handle left content change based on module selection state
- *
- * @param commonProps - Common props for Step Three components
- * @param isModuleClicked - Whether the module selection is clicked
- *
- * @returns The left content JSX element
- */
-export function HandleLeftContentChange(
-  commonProps: StepThreeSubskillsSelectionControllerProps,
-  isModuleClicked: boolean,
-) {
-  if (isModuleClicked) {
-    return <StepThreeSubskillsSelectionController {...commonProps} />;
-  }
-
-  return null!;
-}
 
 /**
  * Change description based on selected sub-skill
