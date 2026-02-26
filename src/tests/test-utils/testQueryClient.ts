5715a445af2aec1903fecbd246159dcda1702465
Adrien <adrienquijo@gmail.com>
2026-02-26 12:02:50 +0100

    Add resetTestQueryClient function to clear shared query client state in tests

--- src/tests/test-utils/testQueryClient.ts
+++ src/tests/test-utils/testQueryClient.ts
@@ -1,3 +1,10 @@
 import { QueryClient } from "@tanstack/react-query";
 
 export const testQueryClient = new QueryClient();
+
+/**
+ * Clears the state of the shared query client used during tests.
+ */
+export function resetTestQueryClient() {
+  testQueryClient.clear();
+}
