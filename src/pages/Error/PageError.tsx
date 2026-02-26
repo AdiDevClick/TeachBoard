70e49e52559d23390385463376074d8a37cd4962
Adrien <adrienquijo@gmail.com>
2026-02-26 11:26:54 +0100

    Refactor PageError component to simplify JSX structure and handle unknown errors

--- src/pages/Error/PageError.tsx
+++ src/pages/Error/PageError.tsx
@@ -9,14 +9,12 @@ export function PageError() {
   // if (error.status === 404) return <Error404 />;
 
   return (
-    <>
-      <div className="error-page" id="error-page">
-        <h1>Oops!</h1>
-        <p>Sorry, an unexpected error has occurred.</p>
-        <p>
-          <i>{error.statusText || error.message}</i>
-        </p>
-      </div>
-    </>
+    <div className="error-page" id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{(error?.statusText || error?.message) ?? "Unknown error"}</i>
+      </p>
+    </div>
   );
 }
