14def70155b1dd72e138c51ae175af8870e05a4f
Adrien <adrienquijo@gmail.com>
2026-02-27 23:55:42 +0100

    Add createComponentName invocation for AvatarsWithLabelAndAddButtonList export

--- src/components/Form/exports/form.exports.ts
+++ src/components/Form/exports/form.exports.ts
@@ -1,5 +1,6 @@
 import { AvatarListWithLabelAndAddButton } from "@/components/Form/AvatarListWithLabelAndAddButton";
 import withListMapper from "@/components/HOCs/withListMapper";
+import { createComponentName } from "@/utils/utils";
 /**
  * @fileoverview This file exports the AvatarListWithLabelAndAddButton component and its variations with HOCs for form control and list mapping.
  */
@@ -9,4 +10,9 @@ import withListMapper from "@/components/HOCs/withListMapper";
  */
 export const AvatarsWithLabelAndAddButtonList = withListMapper(
   AvatarListWithLabelAndAddButton,
+);
+createComponentName(
+  "withListMapper",
+  "AvatarsWithLabelAndAddButtonList",
+  AvatarsWithLabelAndAddButtonList,
 );
