1e7cf66a52624bb327805e0fe2552f187380cc2b
Adrien <adrienquijo@gmail.com>
2026-02-27 23:53:11 +0100

    Refactor PopoverFieldContextType to use CommandSelectionItemProps and PopoverSelectionValue

--- src/api/providers/types/popover.provider.types.ts
+++ src/api/providers/types/popover.provider.types.ts
@@ -1,13 +1,17 @@
-import type {
-  PopoverFieldProps,
-  PopoverFieldState,
-} from "@/components/Popovers/types/popover.types.ts";
+import type { CommandSelectionItemProps } from "@/components/Command/types/command.types";
+import type { PopoverSelectionValue } from "@/components/Popovers/types/popover.types";
 import type { PropsWithChildren } from "react";
 
 /**
  * Props for the Popover Field Provider component
  */
 export type PopoverFieldContextType = {
-  onSelect: PopoverFieldProps["onSelect"];
-  selectedValue?: PopoverFieldState["selectedValue"];
+  /**
+   * This allows consumers to handle the selection event and update their state accordingly.
+   */
+  onSelect: CommandSelectionItemProps["onSelect"];
+  /**
+   * The currently selected value(s). This should be a string for single selection mode, or a Set of strings for multi-selection mode
+   */
+  selectedValue?: PopoverSelectionValue;
 } & PropsWithChildren;
