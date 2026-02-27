953f05a3b1014b85060caa53fbb3e1d0df6255a0
Adrien <adrienquijo@gmail.com>
2026-02-28 00:03:34 +0100

    Refactor exports in NonLabelledGroupItem and EvaluationSlider to include createComponentName for improved clarity and consistency

--- src/components/Selects/non-labelled-item/exports/non-labelled-item-exports.ts
+++ src/components/Selects/non-labelled-item/exports/non-labelled-item-exports.ts
@@ -1,5 +1,6 @@
 import withListMapper from "@/components/HOCs/withListMapper";
 import { NonLabelledGroupItem } from "@/components/Selects/non-labelled-item/NonLabelledGroupItem";
+import { createComponentName } from "@/utils/utils";
 
 /**
  * This file serves as a central export point for the NonLabelledGroupItem component and its related types and utilities. It allows for cleaner imports in other parts of the application by consolidating all exports related to the NonLabelledGroupItem in one place.
@@ -9,3 +10,8 @@ import { NonLabelledGroupItem } from "@/components/Selects/non-labelled-item/Non
  * A version that is enhanced with list mapping capabilities, allowing it to be used in contexts where a list of items needs to be rendered.
  */
 export const NonLabelledGroupItemList = withListMapper(NonLabelledGroupItem);
+createComponentName(
+  "withListMapper",
+  "NonLabelledGroupItemList",
+  NonLabelledGroupItemList,
+);
