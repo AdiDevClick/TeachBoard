f47a86d6a1b5e674c41e12faedd95913d8cf9ac2
Adrien <adrienquijo@gmail.com>
2026-03-02 18:58:42 +0100

    Refactor FormWithDebugProps to include additional properties for improved debugging and form management

--- src/components/Form/types/form.types.ts
+++ src/components/Form/types/form.types.ts
@@ -1,5 +1,6 @@
 import type { SimpleAvatarListProps } from "@/components/Avatar/types/avatar.types.ts";
 import type { SimpleAddButtonWithToolTipProps } from "@/components/Buttons/types/ButtonTypes.ts";
+import type { UseMutationObserverReturn } from "@/hooks/types/use-mutation-observer.types";
 import type { PropsWithChildren } from "react";
 import type {
   FieldErrors,
@@ -20,12 +21,18 @@ export type FormWithDebugProps<T extends FieldValues> = Readonly<
   {
     /** Delay in milliseconds before submitting the form */
     debounceDelay?: number;
+    /** Unique identifier for the form, used for debugging and form management */
     formId: string;
+    /** Form created by react-hook-form */
     form: UseFormReturn<T>;
+    /** Any CSS style for the form */
     className?: string;
+    /** Unique identifier for the page, used for debugging and form management */
+    pageId: string;
     /** Calls a handler when the form is submitted and is valid */
     onValidSubmit: SubmitHandler<T>;
     /** Calls a handler when the form is submitted and is invalid */
     onInvalidSubmit: (e: FieldErrors<T>) => void;
-  } & PropsWithChildren
+  } & PropsWithChildren &
+    UseMutationObserverReturn
 >;
