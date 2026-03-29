a9603ff9ea5b8c1d49279a84c71ceaf5433afcc8
Adrien <adrienquijo@gmail.com>
2026-03-29 09:12:59 +0200

    feat: create central export point for table components with RowsList

---
+++ src/components/Tables/exports/data-table.exports.ts
@@ -0,0 +1,11 @@
+/**
+ * @fileoverview Point d'export central pour les composants Tables.
+ */
+import withListMapper from "@/components/HOCs/withListMapper";
+import { Row } from "@/components/Tables/Row";
+import type { EvaluationItem } from "@/features/evaluations/main/types/evaluations-listing.types";
+
+/**
+ * A list of header groups to the Row component, allowing it to render the table headers.
+ */
+export const RowsList = withListMapper(Row<EvaluationItem>);
