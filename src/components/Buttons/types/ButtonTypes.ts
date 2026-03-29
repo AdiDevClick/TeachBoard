b51c2a350becfd909c1d632fcadf27e1eb42c95c
Adrien <adrienquijo@gmail.com>
2026-03-29 09:12:08 +0200

    feat: enhance button types by adding ComponentType import and defining PaginationButtonProps and CommonTooltipButtonProps

--- src/components/Buttons/types/ButtonTypes.ts
+++ src/components/Buttons/types/ButtonTypes.ts
@@ -1,7 +1,7 @@
 import type { Button } from "@/components/ui/button.tsx";
 import type { LoginButtonsSvgsType } from "@/configs/social.config";
 import type { SafeListMapperProp } from "@/utils/types/types.utils.ts";
-import type { ComponentProps } from "react";
+import type { ComponentProps, ComponentType } from "react";
 
 /**
  * LOGIN BUTTON types
@@ -13,3 +13,19 @@ export type LoginButtonProps = ComponentProps<"button"> &
 
 /** SimpleAddButton types */
 export type SimpleAddButtonProps = ComponentProps<typeof Button>;
+
+/** PaginationButton types */
+export type PaginationButtonProps = Readonly<
+  {
+    label: string;
+    icon: ComponentType;
+  } & ComponentProps<typeof Button>
+>;
+
+/**
+ * COMMON TOOLTIP BUTTON types
+ */
+export type CommonTooltipButtonProps = ComponentProps<typeof Button> & {
+  accessibilityLabel: string;
+  toolTipText?: string;
+};
