d58abfe835e1af6a5508ed3f5f036387af4d5850
Adrien <adrienquijo@gmail.com>
2026-04-15 21:02:15 +0200

    refactor: saveBasicDataToForm va maintenant sauvegarder dès le mount les evaluations data pour s'assurer d'une première passe - Suppression de tout ce qui est relatif aux watchers pour simplifier la vue -

--- src/features/evaluations/create/steps/four/hooks/useStepFourHandler.ts
+++ src/features/evaluations/create/steps/four/hooks/useStepFourHandler.ts
@@ -1,20 +1,18 @@
 import type { UUID } from "@/api/types/openapi/common.types";
 import type { DynamicTagsItemList } from "@/components/Tags/types/tags.types";
 import { API_ENDPOINTS } from "@/configs/api.endpoints.config";
-import { debugLogs } from "@/configs/app-components.config";
 import { useStepFourState } from "@/features/evaluations/create/hooks/useStepFourState";
 import type { UseStepFourHandlerProps } from "@/features/evaluations/create/steps/four/hooks/types/use-step-four-handler.types";
+import { useFormWatchers } from "@/features/evaluations/create/steps/four/hooks/useFormWatchers";
 import {
   type StepFourFormSchema,
   stepFourInputSchema,
 } from "@/features/evaluations/create/steps/four/models/step-four.models";
-import { useEvaluationStepsCreationStore } from "@/features/evaluations/create/store/EvaluationStepsCreationStore";
 import { useEvaluationTableStore } from "@/features/evaluations/main/configs/evaluations.configs";
 import { useCommandHandler } from "@/hooks/database/classes/useCommandHandler";
 import { saveObjectInCache } from "@/hooks/database/fetches/functions/use-fetch.functions";
 import { parseFromObject } from "@/utils/utils";
 import { useEffect, useEffectEvent, useMemo } from "react";
-import { useWatch } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 
@@ -40,8 +38,6 @@ export function useStepFourHandler({
     getAllPresentStudents,
     selectedClass,
     clear,
-    title: titleFromStore,
-    comments: commentsFromStore,
   } = useStepFourState();
 
   const {
@@ -58,40 +54,8 @@ export function useStepFourHandler({
     submitDataReshapeFn,
   });
 
-  const { setValue, reset, control } = form;
+  const { setValue, reset } = form;
   const navigate = useNavigate();
-
-  // Existing watch for overallScore
-  const overallScoreWatch = useWatch({
-    control,
-    name: "overallScore",
-  });
-
-  useWatch({
-    control,
-    name: "title",
-    compute: (title) => {
-      if (
-        (titleFromStore && title !== titleFromStore) ||
-        (!titleFromStore && title)
-      ) {
-        useEvaluationStepsCreationStore.setState({ title });
-      }
-    },
-  });
-
-  useWatch({
-    control,
-    name: "comments",
-    compute: (comments) => {
-      if (
-        (commentsFromStore && comments !== commentsFromStore) ||
-        (!commentsFromStore && comments)
-      ) {
-        useEvaluationStepsCreationStore.setState({ comments });
-      }
-    },
-  });
 
   /**
    * Generates a list of absent students names to display
@@ -139,6 +103,11 @@ export function useStepFourHandler({
         const noAbsentStudents = items?.ids.length === 0;
         const ids = noAbsentStudents ? ["none"] : items?.ids;
 
+        const parsed = parseFromObject(
+          getAllPresentStudents,
+        ) as unknown as StepFourFormSchema["evaluations"];
+
+        setValue("evaluations", parsed ?? []);
         setValue("absence", ids!);
         setValue("classId", classId);
         setValue("attendedModules", modules);
@@ -154,40 +123,6 @@ export function useStepFourHandler({
   useEffect(() => {
     saveBasicDataToForm(selectedClass?.id, presenceMemo);
   }, [presenceMemo, selectedClass?.id]);
-
-  /**
-   * SCORE UPDATES - UPDATE EVALUATIONS IN THE FORM
-   *
-   * @description This is needed to sync the form state with the (default) calculated score -
-   *
-   * @remark If a overall score is manually entered, this will always be the source of truth for the form value, and will become the default value shown in the input.
-   *
-   * @remark The submit to the server may never recalculate the score, but all subskills and module states will be passed on with their own score
-   */
-  const triggerScoreUpdate = useEffectEvent(() => {
-    try {
-      const stringified = JSON.stringify(getAllPresentStudents);
-      const parsed = JSON.parse(stringified);
-
-      setValue("evaluations", parsed);
-    } catch (error) {
-      debugLogs("StepFourController:triggerScoreUpdate", {
-        type: "componentHandler",
-        error,
-        message: "Failed to parse evaluated students for form submission",
-      });
-      setValue("evaluations", []);
-    }
-  });
-
-  /**
-   * SCORE UPDATES -
-   *
-   * @description Each time the overall score changes
-   */
-  useEffect(() => {
-    triggerScoreUpdate();
-  }, [overallScoreWatch]);
 
   /**
    * RESULTS - Show toasts on loading, success and error states
