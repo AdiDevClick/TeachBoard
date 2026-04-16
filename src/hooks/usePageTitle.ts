c753652554720eced9d2596c69c3211078a1242e
Adrien <adrienquijo@gmail.com>
2026-04-16 10:12:27 +0200

    refactor: enhance usePageTitle hook to accept an optional title parameter and improve title management on mount -
    Afin d'éviter des useEffect inutiles dans les composants, le hook peut maintenant insérer un titre dès le mount -
    Pratique quand on connait déjà les datas

--- src/hooks/usePageTitle.ts
+++ src/hooks/usePageTitle.ts
@@ -35,8 +35,10 @@ const pageTitleStore = (() => {
  * @description It uses the route loader data to initialize the title as the original title for the route and allows to edit it.
  *
  * @remarks The title is stored in a shared state so that both the page header and page content can read and update the same title.
+ *
+ * @param title - Optional title to set on mount, if not provided it will use the loader title or default title.
  */
-export function usePageTitle() {
+export function usePageTitle(title?: string) {
   const location = decodeURI(useLocation().pathname);
   const matchingLoader = getMatchingLoader(useMatches, location);
   const routeId = matchingLoader?.id ?? location;
@@ -50,7 +52,37 @@ export function usePageTitle() {
   const isTitleHidden = currentTitle === "hidden";
 
   /**
-   * INIT Trigger -
+   * Set the title for the current route
+   *
+   * @description If the title is set to "default", it will reset to the original title (the one from the loader data or the default title if not set).
+   * Otherwise, it will update the title to the provided value.
+   *
+   * @param title - The new title to set. **default: "default"** (resets to original title)
+   */
+  function setTitle(title: string = "default") {
+    pageTitleStore.setState((prev) => {
+      const { title: previousTitle, originalTitle } =
+        prev.routesTitles.get(routeId) ?? {};
+      const selectedTitle = title === "default" ? currentTitle : title;
+
+      if (selectedTitle === previousTitle) {
+        return prev;
+      }
+
+      return {
+        ...prev,
+        routesTitles: prev.routesTitles
+          .set(routeId, {
+            originalTitle: originalTitle ?? currentTitle,
+            title: selectedTitle,
+          })
+          .clone(),
+      };
+    });
+  }
+
+  /**
+   * 1 - Route Change -
    *
    * @description Makes sure to init the title state -
    * It will save the loader title as the original title for the route and set the current title to it if not already set
@@ -65,38 +97,44 @@ export function usePageTitle() {
   });
 
   /**
-   * INIT -
+   * 1 - Route Change -
+   *
    * @description On route change (id or title change)
    */
   useEffect(() => {
     triggerTitleChange(currentTitle);
   }, [currentTitle, isTitleHidden, routeId]);
 
+  /**
+   * 2 - On Mount -
+   *
+   * @description Sets the title if provided as argument
+   */
+  const triggerTitleOnMount = useEffectEvent((title: string) => {
+    if (title !== undefined) {
+      setTitle(title);
+    }
+  });
+
+  /**
+   * 2 - On Mount -
+   *
+   * @description When the argument title is provided,
+   * it will override the title from the loader on the initial render.
+   *
+   * @remarks This is useful for pages that want to set a specific title on mount that is different from the loader title.
+   */
+  useEffect(() => {
+    if (title !== undefined) {
+      triggerTitleOnMount(title);
+    }
+  }, [title]);
+
   return {
     title: state.routesTitles.get(routeId)?.title,
     isTitleHidden,
     isEditing: state.isEditing,
-    setTitle: (title: string = "default") => {
-      pageTitleStore.setState((prev) => {
-        const { title: previousTitle, originalTitle } =
-          prev.routesTitles.get(routeId) ?? {};
-        const selectedTitle = title === "default" ? currentTitle : title;
-
-        if (selectedTitle === previousTitle) {
-          return prev;
-        }
-
-        return {
-          ...prev,
-          routesTitles: prev.routesTitles
-            .set(routeId, {
-              originalTitle: originalTitle ?? currentTitle,
-              title: selectedTitle,
-            })
-            .clone(),
-        };
-      });
-    },
+    setTitle,
     setIsEditing: (isEdit?: boolean) => {
       pageTitleStore.setState((prev) => ({
         ...prev,
