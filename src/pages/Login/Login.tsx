a483f826b6761535f8de0dd8594dc628d7b4b833
Adrien <adrienquijo@gmail.com>
2026-04-30 19:07:29 +0200

    désactivation pour éviter un bug momentanné

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,7 +2,7 @@ import { useAppStore } from "@/api/store/AppStore";
 import { FieldDescription } from "@/components/ui/field.tsx";
 import { useSidebar } from "@/components/ui/sidebar.tsx";
 import { inputLoginControllers } from "@/features/auth/components/login/forms/login-inputs";
-import LoginView from "@/features/auth/components/login/LoginView";
+import LoginView from "@/features/auth/components/main/LoginView";
 import { useDialog } from "@/hooks/contexts/useDialog.ts";
 import type { LoginPageProps } from "@/pages/Login/types/login-page.types.ts";
 import {
@@ -14,7 +14,7 @@ import {
 } from "@/utils/styles/generic-styles.ts";
 import "@css/GenericPage.scss";
 import { GalleryVerticalEnd } from "lucide-react";
-import { useEffect, useEffectEvent, useLayoutEffect } from "react";
+import { useEffect, useEffectEvent } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 /**
@@ -50,36 +50,32 @@ export function Login({
     closeAllDialogsOnLogin();
   }, []);
 
-  useLayoutEffect(() => {
-    if (isLoggedIn) {
-      navigate("/", { replace: true });
-    }
-  }, [isLoggedIn, navigate]);
-
-  if (isLoggedIn) {
-    return null;
-  }
+  // useLayoutEffect(() => {
+  //   if (isLoggedIn) {
+  //     navigate("/", { replace: true });
+  //   }
+  // }, [isLoggedIn, navigate]);
 
   return (
-    <>
-      {isLoggedIn ? null : (
-        <div {...GENERIC_CONTAINER_STYLE}>
-          <div {...GENERIC_CONTENT_STYLE}>
-            <Link to="/" className={GENERIC_LOGO_STYLE.className}>
-              <div {...GENERIC_LOGO_BACKGROUND_STYLE}>
-                <GalleryVerticalEnd {...GENERIC_LOGO_ICON_STYLE} />
-              </div>
-              Acme Inc.
-            </Link>
-            <LoginView inputControllers={inputControllers} />
-            <FieldDescription className="px-6 text-center">
-              {'En cliquant sur "Se connecter", vous acceptez nos '}
-              <Link to="#">{`Conditions d'utilisation`}</Link> et{" "}
-              <Link to="#">{`Politique de confidentialité`}</Link>.
-            </FieldDescription>
+    // <>
+    //   {isLoggedIn ? null : (
+    <div {...GENERIC_CONTAINER_STYLE}>
+      <div {...GENERIC_CONTENT_STYLE}>
+        <Link to="/" className={GENERIC_LOGO_STYLE.className}>
+          <div {...GENERIC_LOGO_BACKGROUND_STYLE}>
+            <GalleryVerticalEnd {...GENERIC_LOGO_ICON_STYLE} />
           </div>
-        </div>
-      )}
-    </>
+          Acme Inc.
+        </Link>
+        <LoginView inputControllers={inputControllers} />
+        <FieldDescription className="px-6 text-center">
+          {'En cliquant sur "Se connecter", vous acceptez nos '}
+          <Link to="#">{`Conditions d'utilisation`}</Link> et{" "}
+          <Link to="#">{`Politique de confidentialité`}</Link>.
+        </FieldDescription>
+      </div>
+    </div>
+    //   )}
+    // </>
   );
 }
