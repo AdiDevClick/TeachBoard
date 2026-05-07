581af3f0f5ef2553642efc613e5a579448a74dc2
Adrien <adrienquijo@gmail.com>
2026-05-07 15:16:06 +0200

    Refactor: enhance button and icon type definitions, improve invitations feature configurations, and update file downloader types

--- src/configs/social.config.ts
+++ src/configs/social.config.ts
@@ -1,22 +1,13 @@
+import type { LargeButtonWithIconAndLinkProps } from "@/components/Buttons/types/ButtonTypes";
+
 /**
  * Login page social icons.
  */
 export const loginButtonsSvgs = [
-  {
-    label: "Se connecter avec Apple",
-    iconPath: "apple",
-    url: "https://github.com/AdiDevClick",
-  },
-  {
-    label: "Se connecter avec Microsoft",
-    iconPath: "microsoft",
-    url: "https://linkedin.com/in/adrienquijo",
-  },
   {
     label: "Se connecter avec Google",
     iconPath: "google",
     url: "https://accounts.google.com/o/oauth2/v2/auth",
     auth: true,
   },
-];
-export type LoginButtonsSvgsType = (typeof loginButtonsSvgs)[number];
+] satisfies ReadonlyArray<LargeButtonWithIconAndLinkProps>;
