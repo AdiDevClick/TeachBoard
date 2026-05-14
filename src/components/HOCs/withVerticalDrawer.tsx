617689ce942ffd15b880675cbc4f726b02dc6457
Adrien <adrienquijo@gmail.com>
2026-05-14 23:24:59 +0200

    Le HOC a été revu pour n'être là que pour le layout - La partie d'apper du drawer a été sortie du HOC - Il est donc plus aisé de créer un layout et d'utiliser des controlleurs ou vues

--- src/components/HOCs/withVerticalDrawer.tsx
+++ src/components/HOCs/withVerticalDrawer.tsx
@@ -7,16 +7,13 @@ import type {
 import { VerticalDrawerProvider } from "@/api/providers/VerticalDrawer.provider";
 import { Button } from "@/components/ui/button";
 import {
-  Drawer,
   DrawerClose,
-  DrawerContent,
   DrawerDescription,
   DrawerFooter,
   DrawerHeader,
   DrawerTitle,
 } from "@/components/ui/drawer";
 import { useVerticalDrawer } from "@/hooks/contexts/useVerticalDrawer";
-import { useIsMobile } from "@/hooks/use-mobile";
 import { createNameForHOC } from "@/utils/utils";
 import { type ComponentType } from "react";
 
@@ -24,11 +21,12 @@ import { type ComponentType } from "react";
  * Higher-Order Component that wraps a given component with a vertical drawer layout.
  * It provides a consistent structure for drawers across the application, including a header, content area, and footer.
  *
+ * @description The `withVerticalDrawer` HOC uses Compound design pattern to allow for flexible composition of the drawer's header, content, and footer.
+ *
+ * @param WrappedContent - The component to be rendered inside the drawer's content area. This component will receive any additional props passed to the `VerticalDrawer.Content` component.
  * @param drawerContent - Props for the DrawerContent component, which can include any additional content-specific properties.
  * @param drawerHeader - Props for the DrawerHeader component, including title and description.
  * @param drawerFooter - Props for the DrawerFooter component, including a close button configuration.
- * @param open - A boolean that controls whether the drawer is open or closed.
- * @param onClose - A callback function that is called when the drawer is closed.
  * @param children - The content to be rendered inside the drawer, typically the wrapped component.
  */
 export function withVerticalDrawer<P extends object>(
@@ -38,32 +36,13 @@ export function withVerticalDrawer<P extends object>(
     drawerContent,
     drawerFooter,
     drawerHeader,
-    drawerContentProps,
     children,
-    open = false,
-    onClose,
-    ...drawer
   }: VerticalDrawerProps<P>) {
-    const isMobile = useIsMobile();
-
-    const onOpenChangeHandler = (open: boolean) => {
-      if (!open) {
-        onClose?.();
-      }
-    };
-
     return (
       <VerticalDrawerProvider
         value={{ drawerContent, drawerHeader, drawerFooter }}
       >
-        <Drawer
-          open={open}
-          onOpenChange={onOpenChangeHandler}
-          direction={isMobile ? "bottom" : "right"}
-          {...drawer}
-        >
-          <DrawerContent {...drawerContentProps}>{children}</DrawerContent>
-        </Drawer>
+        {children}
       </VerticalDrawerProvider>
     );
   }
@@ -81,8 +60,10 @@ export function withVerticalDrawer<P extends object>(
 
     return (
       <DrawerHeader {...headerProps}>
-        <DrawerTitle {...titleProps}>{label}</DrawerTitle>
-        <DrawerDescription {...descriptionProps}>{desc}</DrawerDescription>
+        <DrawerTitle {...titleProps}>{label ?? "Détails"}</DrawerTitle>
+        <DrawerDescription {...descriptionProps}>
+          {desc ?? "En cours de récupération..."}
+        </DrawerDescription>
         {children}
       </DrawerHeader>
     );
