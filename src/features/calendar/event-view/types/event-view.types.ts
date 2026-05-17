7663097cefad7b651ea8d9855f64c012f4f11fcb
Adrien <adrienquijo@gmail.com>
2026-05-17 23:09:41 +0200

    fix(EventViewProps): format type definition for clarity

--- src/features/calendar/event-view/types/event-view.types.ts
+++ src/features/calendar/event-view/types/event-view.types.ts
@@ -5,7 +5,7 @@ import type { PageWithControllers } from "@/types/AppPagesInterface";
  * Types definition for the EventView component, which displays the details of a calendar event in a vertical drawer.
  */
 export type EventViewProps = Readonly<
-  Omit<PageWithControllers, "inputControllers"> & {
-    inputControllers?: typeof eventInputs;
-  }
->;
+  Omit<PageWithControllers, "inputControllers">
+> & {
+  inputControllers?: typeof eventInputs;
+};
