1b5af6dde6dd202192b596b31ab7aa034c7b928c
Adrien <adrienquijo@gmail.com>
2026-05-17 23:07:17 +0200

    feat(CalendarEvent): refactor event props to use WithChildren and WithEvent types

--- src/components/Sidebar/calendar/Event/types/calendar-events.types.ts
+++ src/components/Sidebar/calendar/Event/types/calendar-events.types.ts
@@ -7,24 +7,34 @@
  */
 
 import type { Button } from "@/components/ui/button";
+import type { Prettify } from "@/utils/types/types.utils";
 import type { Event } from "@microsoft/microsoft-graph-types";
 import type { ComponentProps, MouseEvent } from "react";
+
+export type WithChildren = Prettify<
+  Readonly<ComponentProps<typeof Button> & { event?: never }>
+>;
+
+export type WithEvent = Prettify<
+  Readonly<
+    {
+      children?: never;
+      event: Event;
+      /**
+       * Optional click handler - Receives the mouse event and the calendar event data as parameters.
+       * This allows parent components to handle event clicks, such as opening a detailed view of the event.
+       */
+      onClick?: (
+        /** The mouse event - Classic React event */
+        e: MouseEvent<HTMLButtonElement>,
+        /** The calendar event data - @see Event */
+        event: Event,
+      ) => void;
+    } & Omit<ComponentProps<typeof Button>, "onClick">
+  >
+>;
 
 /**
  * Props for a calendar event displayed in the sidebar calendar.
  */
-export type CalendarEventProps = Readonly<
-  {
-    event: Event;
-    /**
-     * Optional click handler - Receives the mouse event and the calendar event data as parameters.
-     * This allows parent components to handle event clicks, such as opening a detailed view of the event.
-     */
-    onClick?: (
-      /** The mouse event - Classic React event */
-      e: MouseEvent<HTMLButtonElement>,
-      /** The calendar event data - @see Event */
-      event: Event,
-    ) => void;
-  } & Omit<ComponentProps<typeof Button>, "onClick">
->;
+export type CalendarEventProps = Readonly<WithChildren | WithEvent>;
