352628789908bc357c1727d3433fdafa028053a6
Adrien <adrienquijo@gmail.com>
2026-05-18 20:58:42 +0200

    feat(EventViewController): add optional event prop to EventViewControllerProps

--- src/features/calendar/event-view/controllers/types/event-view.controller.types.ts
+++ src/features/calendar/event-view/controllers/types/event-view.controller.types.ts
@@ -1,6 +1,7 @@
 import type { EventViewFormSchema } from "@/features/calendar/event-view/models/event-view.models";
 import type { EventViewProps } from "@/features/calendar/event-view/types/event-view.types";
 import type { AppControllerInterface } from "@/types/AppControllerInterface";
+import type { Event } from "@microsoft/microsoft-graph-types";
 
 /**
  * Props for the EventViewController component.
@@ -8,5 +9,7 @@ import type { AppControllerInterface } from "@/types/AppControllerInterface";
 export type EventViewControllerProps = Readonly<
   AppControllerInterface<EventViewFormSchema> & {
     inputControllers: NonNullable<EventViewProps["inputControllers"]>;
+    /** The event data for hydration */
+    event?: Event;
   }
 >;
