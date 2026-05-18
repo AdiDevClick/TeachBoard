3bd38020f534856e0992014d28fe9041d1219cc8
Adrien <adrienquijo@gmail.com>
2026-05-18 10:29:21 +0200

    feat(useDisabledForSubmit): add custom hook to manage submit button disabled state based on form validation

---
+++ src/hooks/useDisabledForSubmit.ts
@@ -0,0 +1,34 @@
+import { useMemo } from "react";
+import type { UseFormReturn } from "react-hook-form";
+
+export type UseDisabledForSubmitOptions = UseFormReturn["formState"];
+/**
+ * Custom hook to determine if a submit button should be disabled based on the form state.
+ *
+ * @description The submit button will be disabled if any of the following conditions are true:
+ * - The form has been submitted successfully.
+ * - The form is currently being submitted.
+ * - There are any validation errors present in the form.
+ * - The form is not valid.
+ * - The form is currently being validated.
+ *
+ * @param formState - The form state object from react-hook-form.
+ * @return A boolean indicating whether the submit button should be disabled.
+ */
+export function useDisabledForSubmit(formState?: UseDisabledForSubmitOptions) {
+  const isDisabled = useMemo(() => {
+    if (!formState) return false;
+
+    const hasAnyErrors = Object.keys(formState.errors).length > 0;
+
+    return (
+      formState.isSubmitSuccessful ||
+      formState.isSubmitting ||
+      hasAnyErrors ||
+      !formState.isValid ||
+      formState.isValidating
+    );
+  }, [formState]);
+
+  return isDisabled;
+}
