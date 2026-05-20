99146a03d67fc7fbfa985590948b5b8ae4ca43fb
Adrien <adrienquijo@gmail.com>
2026-05-20 23:18:53 +0200

    feat(useCalendar): add TypeScript types for submitRoute, submitDataReshapeFn, and pageId options

--- src/components/Sidebar/calendar/hooks/types/use-calendar.types.ts
+++ src/components/Sidebar/calendar/hooks/types/use-calendar.types.ts
@@ -6,6 +6,12 @@
  * - The `UseCalendarOptions` type defines the options that can be passed to the `useCalendar` hook, including the initial date and optional fetch configuration.
  */
 
+import type { AppDialogNames } from "@/configs/app.config";
+import type {
+  ApiEndpointType,
+  DataReshapeFn,
+} from "@/types/AppInputControllerInterface";
+
 /**
  * Defines the allowed values for the range of calendar data to fetch, which can be "month", "week", or "day".
  */
@@ -28,4 +34,18 @@ export type UseCalendarOptions = {
     start: Date;
     type: CalendarFetchRange;
   };
+  /**
+   * Optional string representing the API endpoint to which calendar event data should be submitted when creating or updating events
+   */
+  submitRoute?: ApiEndpointType;
+  /**
+   * Optional function to reshape the calendar event data before submission, allowing for custom formatting or additional data manipulation as needed.
+   */
+  submitDataReshapeFn?: DataReshapeFn;
+  /**
+   * Optional string representing the page ID to be used in the command handler for fetching and submitting calendar event data, allowing for context-specific handling of calendar-related actions within the application.
+   *
+   * @default "none"
+   */
+  pageId?: AppDialogNames;
 };
