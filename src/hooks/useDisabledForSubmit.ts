4de1d0481ebb1b1d218f5fa345b8e2e08d044b83
Adrien <adrienquijo@gmail.com>
2026-05-20 23:36:00 +0200

    feat(hooks): refactor useDisabledForSubmit to accept form state and options for improved flexibility

--- src/hooks/useDisabledForSubmit.ts
+++ src/hooks/useDisabledForSubmit.ts
@@ -1,5 +1,10 @@
-import type { UseDisabledForSubmitOptions } from "@/hooks/types/use-disabled-for-submit.types";
-import { useMemo } from "react";
+import {
+  useForm,
+  useFormState,
+  type FieldValues,
+  type FormState,
+  type UseFormReturn,
+} from "react-hook-form";
 
 /**
  * Custom hook to determine if a submit button should be disabled based on the form state.
@@ -14,20 +19,28 @@ import { useMemo } from "react";
  * @param formState - The form state object from react-hook-form.
  * @return A boolean indicating whether the submit button should be disabled.
  */
-export function useDisabledForSubmit(formState?: UseDisabledForSubmitOptions) {
-  const isDisabled = useMemo(() => {
-    if (!formState) return false;
+export function useDisabledForSubmit(
+  form: UseFormReturn<FieldValues> | FormState<FieldValues>,
+  options?: { forceSubscribe?: boolean },
+) {
+  const fallbackForm = useForm<FieldValues>();
+  const hasControl = "control" in form;
+  const formState = hasControl ? form.formState : form;
 
-    const hasAnyErrors = Object.keys(formState.errors).length > 0;
+  const forcedSubscribeProps = useFormState({
+    control: hasControl ? form.control : fallbackForm.control,
+  });
 
-    return (
-      formState.isSubmitSuccessful ||
-      formState.isSubmitting ||
-      hasAnyErrors ||
-      !formState.isValid ||
-      formState.isValidating
-    );
-  }, [formState]);
+  const formVariable =
+    options?.forceSubscribe && hasControl ? forcedSubscribeProps : formState;
 
-  return isDisabled;
+  const hasAnyErrors = Object.keys(formVariable.errors).length > 0;
+
+  return (
+    formVariable.isSubmitSuccessful ||
+    formVariable.isSubmitting ||
+    hasAnyErrors ||
+    !formVariable.isValid ||
+    formVariable.isValidating
+  );
 }
