8bd32435637af9f9052a6964d95850b015f88ba7
Adrien <adrienquijo@gmail.com>
2026-05-20 23:27:22 +0200

    feat(event-view.inputs): update event input properties and remove unused fields

--- src/features/calendar/event-view/form/event-view.inputs.ts
+++ src/features/calendar/event-view/form/event-view.inputs.ts
@@ -8,37 +8,26 @@ const eventSubject = {
   type: "text",
   placeholder: "Ex: Le titre de l'événement...",
   autoComplete: "off",
-  // apiEndpoint: API_ENDPOINTS.POST.CALENDAR_EVENT.endpoints.MAIN,
-  // task: "event-subject",
   required: true,
 } satisfies EventViewInputItem;
 
 const eventIsAllDay = {
   name: "isAllDay",
   title: "Toute la journée",
-  type: "checkbox",
-  // task: "event-isAllDay",
-  // apiEndpoint: API_ENDPOINTS.POST.CALENDAR_EVENT.endpoints.MAIN,
-  required: false,
+  required: true,
 } satisfies EventViewInputItem;
 
 const eventTimeRange = {
   start: {
     name: "start",
     label: "Heure de début",
-    // type: "time",
     step: timeSteps,
-    // apiEndpoint: API_ENDPOINTS.POST.CALENDAR_EVENT.endpoints.MAIN,
-    // task: "event-start",
     required: false,
   },
   end: {
     name: "end",
     label: "Heure de fin",
     step: timeSteps,
-    // type: "time",
-    // apiEndpoint: API_ENDPOINTS.POST.CALENDAR_EVENT.endpoints.MAIN,
-    // task: "event-end",
     required: false,
   },
 } satisfies {
@@ -50,12 +39,9 @@ const date: EventViewInputItem & Omit<DateFieldProps, "control"> = {
   name: "date",
   label: "Date",
   mode: "single",
-  // multiSelection: true,
   placeholder: "Sélectionnez une date",
   fullWidth: true,
-  // apiEndpoint: API_ENDPOINTS.POST.CALENDAR_EVENT.endpoints.MAIN,
-  // task: "event-date",
-  required: false,
+  required: true,
 };
 
 const bodyContent = {
@@ -63,8 +49,6 @@ const bodyContent = {
   title: "Description (optionnelle)",
   type: "text",
   placeholder: "Ex: Description de l'événement...",
-  // apiEndpoint: API_ENDPOINTS.POST.CALENDAR_EVENT.endpoints.MAIN,
-  // task: "event-content",
   required: false,
 } satisfies EventViewInputItem;
 
